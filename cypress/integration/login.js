e2a873c5eb6fcf14c0f47c74268cd12f5e3c110b
JevgenijaMa <eugenijam@gmail.com>
2019-07-24 12:56:29 +0300

    verify current date

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -12,16 +12,17 @@ describe('Sourcebooks login', function() {
 
         cy.visit('/');
         cy.get('[id="loginForm.userId"]').click({force:true});
-        cy.get('[aria-label="Demo User"]').click();
+        cy.get('[aria-label="Jevgenija Malomuž"]').click();
         cy.get('[id="loginForm.role"]').click({force:true});
         cy.get('[aria-label="User"]').click();
         cy.get('[type="submit"]').click();
 
         cy.url().should('include', '/time-logging');
+        cy.get('.calendar--today').contains('24')
         cy.get('.page__title').contains('Timesheets')
         cy.get('.calendar').should('be.visible')
         cy.get('.tile.form').should('be.visible')
-        cy.get('.user-info__title').contains('Demo User');
+        cy.get('.user-info__title').contains('Jevgenija Malomuž');
         cy.get('.main-nav').find('li').should('have.length', 1);
     })
 })
