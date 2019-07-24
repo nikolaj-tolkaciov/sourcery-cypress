0652f0f234210a37fad84d426575e0ddb3a38f54
astarad <asta_cibulskaite@yahoo.com>
2019-07-24 12:56:18 +0300

    TC1- feature added

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -12,16 +12,18 @@ describe('Sourcebooks login', function() {
 
         cy.visit('/');
         cy.get('[id="loginForm.userId"]').click({force:true});
-        cy.get('[aria-label="Demo User"]').click();
+        cy.get('[aria-label="Asta Radžiuvienė"]').click();
         cy.get('[id="loginForm.role"]').click({force:true});
-        cy.get('[aria-label="User"]').click();
+        cy.get('[aria-label="Team Lead"]').click();
         cy.get('[type="submit"]').click();
 
         cy.url().should('include', '/time-logging');
         cy.get('.page__title').contains('Timesheets')
         cy.get('.calendar').should('be.visible')
         cy.get('.tile.form').should('be.visible')
-        cy.get('.user-info__title').contains('Demo User');
-        cy.get('.main-nav').find('li').should('have.length', 1);
+        cy.get('.user-info__title').contains('Asta Radžiuviene');
+        cy.get('.main-nav').find('li').should('have.length', 2);
+        cy.get('.calendar--today').contains('24');
     })
 })
+
