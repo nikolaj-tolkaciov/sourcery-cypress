a93dd92abb49d93888b576290f5980d5984cd231
lawrynuxaz <lauris4@gmail.com>
2019-07-24 16:16:30 +0300

    user and admin login procedures

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -26,4 +26,22 @@ describe('Sourcebooks login', function() {
         cy.get('.main-nav').find('li').should('have.length', 1);
         cy.get('.main-nav__link--active').should('have.css', 'color', 'rgb(64, 76, 237)');
     })
+
+        it('Should be able to login with role Admin', function () {
+        cy.visit('/');
+        cy.get('[id="loginForm.userId"]').click({force:true});
+        cy.get('[aria-label="Laurynas Kochas"]').click();
+        cy.get('[id="loginForm.role"]').click({force:true});
+        cy.get('[aria-label="Admin"]').click();
+        cy.get('[type="submit"]').click();
+
+        cy.url().should('include', '/time-logging');
+        cy.get('.page__title').contains('Timesheets');
+        cy.get('.calendar').should('be.visible');
+        cy.get('.calendar').find('.calendar--selected').contains('24');
+        cy.get('.tile.form').should('be.visible');
+        cy.get('.user-info__title').contains('Laurynas Kochas');
+        cy.get('.main-nav').find('li').should('have.length', 6);
+        cy.get('.main-nav__link--active').should('have.css', 'color', 'rgb(64, 76, 237)');
+    })
 })
