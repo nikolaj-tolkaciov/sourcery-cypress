04fd70370c8b42f28023e94ef25e4e9785bf8c4f
AlgimantasV <algimantasviz@yahoo.com>
2019-07-24 14:54:45 +0300

    changed hard coded date value to getDate

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -23,7 +23,7 @@ describe('Sourcebooks login', function() {
         cy.get('.tile.form').should('be.visible')
         cy.get('.user-info__title').contains('Algimantas Vizgirda');
         cy.get('.main-nav').find('li').should('have.length', 1);
-        cy.get('.calendar--today').contains('24');
+        cy.get('.calendar--today').contains(new Date().getDate());
 
     })
 })
