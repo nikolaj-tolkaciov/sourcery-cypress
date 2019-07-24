22ac105744805fb3e5f9d3ac6652f3280c44c788
Lansing12 <benediktas.gilis@gmail.com>
2019-07-24 15:59:47 +0300

    Added Test case 2

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -25,4 +25,19 @@ describe('Sourcebooks login', function() {
         cy.get('.main-nav').find('li').should('have.length', 1);
         cy.get('.calendar--today').contains(new Date().getDate());
     })
+
+    it('Verify all user roles can log in and should see appropriate tabs', function () {
+
+        cy.visit('/');
+        cy.get('[id="loginForm.userId"]').click({force:true});
+        cy.get('[aria-label="Benediktas Gilis"]').click();
+        cy.get('[id="loginForm.role"]').click({force:true});
+        cy.get('[aria-label="User"]').click();
+        cy.get('[type="submit"]').click();
+        cy.url().should('include', '/time-logging');
+        cy.get('.page__title').contains('Timesheets')
+        cy.get('.user-info__title').contains('Benediktas Gilis');
+        cy.get('.main-nav').find('li').should('have.length', 1);
+        cy.get('.main-nav__link--active').should('have.css', 'color','rgb(64, 76, 237)');
+    })
 })
