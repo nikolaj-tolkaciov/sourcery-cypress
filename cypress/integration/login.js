d3584b27d55b9c24dc62a919dd488331e6300063
bertarejabut <bertarejabut@gmail.com>
2019-07-24 15:52:44 +0300

    All user roles can log in.

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -7,22 +7,24 @@ describe('Sourcebooks login', function() {
         cy.get('[type="submit"]').click();
         cy.get('.Select.not-valid').should('be.visible')
     })
+    const roles = [['User', 1], ['Team Lead', 2], ['Manager', 5], ['Accountant',5], ['Admin',6]];
+    for(let x = 0; x < 5; x++){
+        it('All user roles can log in', function () {
+            cy.visit('/');
+            cy.get('[id="loginForm.userId"]').click({force:true});               
+            cy.get('[aria-label="Berta Rėja Butvilaitė"]').click();
+            cy.get('[id="loginForm.role"]').click({force:true});
+            cy.get('[aria-label="' + roles[x][0] + '"]').click();
+            cy.get('[type="submit"]').click();
 
-    it('Should be able to see today\'s date marked on calendar', function () {
-
-        cy.visit('/');
-        cy.get('[id="loginForm.userId"]').click({force:true});
-        cy.get('[aria-label="Berta Rėja Butvilaitė"]').click();
-        cy.get('[id="loginForm.role"]').click({force:true});
-        cy.get('[aria-label="User"]').click();
-        cy.get('[type="submit"]').click();
-
-        cy.url().should('include', '/time-logging');
-        cy.get('.page__title').contains('Timesheets')
-        cy.get('.calendar').should('be.visible')
-        cy.get('.tile.form').should('be.visible')
-        cy.get('.user-info__title').contains('Berta Reja Butvilaite');
-        cy.get('.main-nav').find('li').should('have.length', 1);
-        cy.get('.calendar--today').contains(new Date().getDate());
-    })
+            cy.url().should('include', '/time-logging');
+            cy.get('.page__title').contains('Timesheets');
+            cy.get('.calendar').should('be.visible');
+            cy.get('.tile.form').should('be.visible');
+            cy.get('.user-info__title').contains('Berta Reja Butvilaite');
+            cy.get('.main-nav').find('li').should('have.length', roles[x][1]);
+            cy.get('.calendar--today').contains(new Date().getDate());
+            cy.get('.main-nav__link--active').should('have.css', 'color', 'rgb(64, 76, 237)');
+        })
+    }
 })
