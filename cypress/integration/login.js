45ec858c0321c26962ee2deb24f417c95f572b93
justinaburm <justinaburm@gmail.com>
2019-07-24 12:29:56 +0300

    Try to login with personal user account and check if the day of today is selected in the calendar view.

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -8,11 +8,11 @@ describe('Sourcebooks login', function() {
         cy.get('.Select.not-valid').should('be.visible')
     })
 
-    it('Should be able to login with role User', function () {
+    it('Should be able to login with role User individual User account', function () {
 
         cy.visit('/');
         cy.get('[id="loginForm.userId"]').click({force:true});
-        cy.get('[aria-label="Demo User"]').click();
+        cy.get('[aria-label="Justina Burmistrovaitė"]').click();
         cy.get('[id="loginForm.role"]').click({force:true});
         cy.get('[aria-label="User"]').click();
         cy.get('[type="submit"]').click();
@@ -21,7 +21,8 @@ describe('Sourcebooks login', function() {
         cy.get('.page__title').contains('Timesheets')
         cy.get('.calendar').should('be.visible')
         cy.get('.tile.form').should('be.visible')
-        cy.get('.user-info__title').contains('Demo User');
+        cy.get('.user-info__title').contains('Justina');
         cy.get('.main-nav').find('li').should('have.length', 1);
+        cy.get('.calendar').find('.calendar--selected').contains('24');
     })
 })
