11e3255e2f2b21359f6b8ba7148c657d3de08dc7
lawrynuxaz <lauris4@gmail.com>
2019-07-24 16:27:32 +0300

    All roles tested

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -44,4 +44,62 @@ describe('Sourcebooks login', function() {
         cy.get('.main-nav').find('li').should('have.length', 6);
         cy.get('.main-nav__link--active').should('have.css', 'color', 'rgb(64, 76, 237)');
     })
+
+
+    it('Should be able to login with role Team Lead', function () {
+        cy.visit('/');
+        cy.get('[id="loginForm.userId"]').click({force:true});
+        cy.get('[aria-label="Laurynas Kochas"]').click();
+        cy.get('[id="loginForm.role"]').click({force:true});
+        cy.get('[aria-label="Team Lead"]').click();
+        cy.get('[type="submit"]').click();
+
+        cy.url().should('include', '/time-logging');
+        cy.get('.page__title').contains('Timesheets');
+        cy.get('.calendar').should('be.visible');
+        cy.get('.calendar').find('.calendar--selected').contains('24');
+        cy.get('.tile.form').should('be.visible');
+        cy.get('.user-info__title').contains('Laurynas Kochas');
+        cy.get('.main-nav').find('li').should('have.length', 2);
+        cy.get('.main-nav__link--active').should('have.css', 'color', 'rgb(64, 76, 237)');
+
+})
+
+it('Should be able to login with role Manager', function () {
+    cy.visit('/');
+    cy.get('[id="loginForm.userId"]').click({force:true});
+    cy.get('[aria-label="Laurynas Kochas"]').click();
+    cy.get('[id="loginForm.role"]').click({force:true});
+    cy.get('[aria-label="Manager"]').click();
+    cy.get('[type="submit"]').click();
+
+    cy.url().should('include', '/time-logging');
+    cy.get('.page__title').contains('Timesheets');
+    cy.get('.calendar').should('be.visible');
+    cy.get('.calendar').find('.calendar--selected').contains('24');
+    cy.get('.tile.form').should('be.visible');
+    cy.get('.user-info__title').contains('Laurynas Kochas');
+    cy.get('.main-nav').find('li').should('have.length', 5);
+    cy.get('.main-nav__link--active').should('have.css', 'color', 'rgb(64, 76, 237)');
+
+})
+it('Should be able to login with role Accountant', function () {
+    cy.visit('/');
+    cy.get('[id="loginForm.userId"]').click({force:true});
+    cy.get('[aria-label="Laurynas Kochas"]').click();
+    cy.get('[id="loginForm.role"]').click({force:true});
+    cy.get('[aria-label="Accountant"]').click();
+    cy.get('[type="submit"]').click();
+
+    cy.url().should('include', '/time-logging');
+    cy.get('.page__title').contains('Timesheets');
+    cy.get('.calendar').should('be.visible');
+    cy.get('.calendar').find('.calendar--selected').contains('24');
+    cy.get('.tile.form').should('be.visible');
+    cy.get('.user-info__title').contains('Laurynas Kochas');
+    cy.get('.main-nav').find('li').should('have.length', 5);
+    cy.get('.main-nav__link--active').should('have.css', 'color', 'rgb(64, 76, 237)');
+
+})
+
 })
