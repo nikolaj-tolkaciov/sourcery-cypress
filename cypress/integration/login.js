280df6d04d59f44ce23ae54d124e5bac7eaea293
Cheekiuz <ugne.lekstutiene@gmail.com>
2019-07-24 14:57:08 +0300

    After second change

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -17,10 +17,10 @@ describe('Sourcebooks login', function() {
         cy.get('[aria-label="User"]').click();
         cy.get('[type="submit"]').click();
 
-        cy.url().should('include', '/time-logging');
-        cy.get('.calendar--today').contains(new Date().getDate());
+        cy.url().should('include', '/time-logging');        
         cy.get('.page__title').contains('Timesheets')
         cy.get('.calendar').should('be.visible')
+        cy.get('.calendar--today').contains(new Date().getDate());
         cy.get('.tile.form').should('be.visible')
         cy.get('.user-info__title').contains('Ugne Lekstutiene');
         cy.get('.main-nav').find('li').should('have.length', 1);
