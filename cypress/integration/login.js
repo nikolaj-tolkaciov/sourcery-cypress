40df0fe0a8ee3dc493d80e532fb67cf6e9c29421
justinaburm <justinaburm@gmail.com>
2019-07-24 14:17:10 +0300

    changed the date test to be flexible and be able to test it on any day of the month, +added missing last name of the user

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -8,7 +8,7 @@ describe('Sourcebooks login', function() {
         cy.get('.Select.not-valid').should('be.visible')
     })
 
-    it('Should be able to login with role User individual User account', function () {
+    it('Should be able to login with role User individual User', function () {
 
         cy.visit('/');
         cy.get('[id="loginForm.userId"]').click({force:true});
@@ -21,8 +21,8 @@ describe('Sourcebooks login', function() {
         cy.get('.page__title').contains('Timesheets')
         cy.get('.calendar').should('be.visible')
         cy.get('.tile.form').should('be.visible')
-        cy.get('.user-info__title').contains('Justina');
+        cy.get('.user-info__title').contains('Justina Burmistrovaite');
         cy.get('.main-nav').find('li').should('have.length', 1);
-        cy.get('.calendar').find('.calendar--selected').contains('24');
+        cy.get('.calendar').find('.calendar--selected').contains(new Date().getDate());
     })
 })
