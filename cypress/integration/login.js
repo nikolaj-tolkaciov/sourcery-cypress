426fdc08decffc3a92e6797bba1138a3fedebf0a
AlgimantasV <algimantasviz@yahoo.com>
2019-07-25 09:38:58 +0300

    corrected test name in VS

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -8,7 +8,7 @@ describe('Sourcebooks login', function() {
         cy.get('.Select.not-valid').should('be.visible')
     })
 
-    it('Should be able to login with role User and check if today's_date_is_selected', function () {
+    it('Should be able to login with role User and check if today date is selected', function () {
 
         cy.visit('/');
         cy.get('[id="loginForm.userId"]').click({force:true});
