b899a9972b99bf814e6e21bc4257d759e76e7a42
Dovile <dovmartink@gmail.com>
2019-07-25 09:59:33 +0300

    Change user roles and check color

--- cypress/integration/user_roles.js
+++ cypress/integration/user_roles.js
@@ -8,111 +8,31 @@ describe('Sourcebooks login', function() {
         cy.get('.Select.not-valid').should('be.visible')
     })
 
-    it('Should be able to login with role User', function () {
+    const roles= [['User', 1], ["Team Lead", 2], ['Manager', 5], ['Accountant', 5], ['Admin', 6]]
 
-        cy.visit('/');
-        cy.get('[id="loginForm.userId"]').click({force:true});
-        cy.get('[aria-label="Dovilė Martinkutė"]').click();
-        cy.get('[id="loginForm.role"]').click({force:true});
-        cy.get('[aria-label="User"]').click();                          //change roles
-        cy.get('[type="submit"]').click();
+    for (let i = 0; i < roles.length; i++) {
 
-        cy.url().should('include', '/time-logging');         
+        it('Should be able to login with each role User', function () {
 
-        cy.get('.page__title').contains('Timesheets')
-        cy.get('.calendar').should('be.visible')
-        cy.get('.tile.form').should('be.visible')
-        cy.get('.user-info__title').contains('Dovile Martinkute');
-        cy.get('.main-nav').find('li').should('have.length', 1);                //changes according to role
-        cy.get('.calendar__date').contains(new Date().getDate());
-        cy.get('[aria-current = true]').contains("Time Logging");
-        cy.get('[aria-current = true]').should('have.css', 'color', 'rgb(64, 76, 237)');
-        //cy.get('.main-nav__link--active').should('have.css', 'color', 'rgb(64, 76, 237)');
+                cy.visit('/');
+                cy.get('[id="loginForm.userId"]').click({force:true});
+                cy.get('[aria-label="Dovilė Martinkutė"]').click();
+                cy.get('[id="loginForm.role"]').click({force:true});
+                cy.get(`[aria-label="${roles[i][0]}"]`).click();
+                cy.get('[type="submit"]').click();
 
+                cy.url().should('include', '/time-logging');
+                cy.get('.page__title').contains('Timesheets')
+                cy.get('.calendar').should('be.visible')
+                cy.get('.tile.form').should('be.visible')
+                cy.get('.user-info__title').contains('Dovile Martinkute');
+                cy.get('.main-nav').find('li').should('have.length', roles[i][1]);
+                cy.get('.calendar__date').contains(new Date().getDate());
+                cy.get('.main-nav__link--active').should('have.css', 'color', 'rgb(64, 76, 237)');
+                cy.get('.user-info__title').click({force:true});    //logout
+                cy.get('[id="logout-button"]').click({force:true});
 
-//lougout
-cy.get('.user-info__title').click({force:true});
-cy.get('[id="logout-button"]').click();
+        })
 
-//next user team lead
-        cy.get('[id="loginForm.userId"]').click({force:true});
-        cy.get('[aria-label="Dovilė Martinkutė"]').click();
-        cy.get('[id="loginForm.role"]').click({force:true});
-        cy.get('[aria-label="Team Lead"]').click();                          //change roles
-        cy.get('[type="submit"]').click();
-
-        cy.url().should('include', '/time-logging');         
-
-        cy.get('.calendar').should('be.visible')
-        cy.get('.tile.form').should('be.visible')
-        cy.get('.user-info__title').contains('Dovile Martinkute');
-        cy.get('.main-nav').find('li').should('have.length', 2);                //changes according to role
-        cy.get('.calendar__date').contains(new Date().getDate());
-        cy.get('[aria-current = true]').contains("Time Logging");
-        cy.get('[aria-current = true]').should('have.css', 'color', 'rgb(64, 76, 237)');
-
-
-//lougout
-cy.get('.user-info__title').click({force:true});
-cy.get('[id="logout-button"]').click({force:true});
-
-
-//next user manager
-cy.get('[id="loginForm.userId"]').click({force:true});
-cy.get('[aria-label="Dovilė Martinkutė"]').click();
-cy.get('[id="loginForm.role"]').click({force:true});
-cy.get('[aria-label="Manager"]').click();                          //change roles
-cy.get('[type="submit"]').click();
-
-cy.url().should('include', '/time-logging');         
-
-cy.get('.calendar').should('be.visible')
-cy.get('.tile.form').should('be.visible')
-cy.get('.user-info__title').contains('Dovile Martinkute');
-cy.get('.main-nav').find('li').should('have.length', 5);                //changes according to role
-cy.get('[aria-current = true]').contains("Time Logging");
-cy.get('[aria-current = true]').should('have.css', 'color', 'rgb(64, 76, 237)');
-
-//lougout
-cy.get('.user-info__title').click({force:true});
-cy.get('[id="logout-button"]').click({force:true});
-
-//next user accountant
-cy.get('[id="loginForm.userId"]').click({force:true});
-cy.get('[aria-label="Dovilė Martinkutė"]').click();
-cy.get('[id="loginForm.role"]').click({force:true});
-cy.get('[aria-label="Accountant"]').click();                          //change roles
-cy.get('[type="submit"]').click();
-
-cy.url().should('include', '/time-logging');         
-
-cy.get('.calendar').should('be.visible')
-cy.get('.tile.form').should('be.visible')
-cy.get('.user-info__title').contains('Dovile Martinkute');
-cy.get('.main-nav').find('li').should('have.length', 5);                //changes according to role
-
-cy.get('[aria-current = true]').contains("Time Logging");
-cy.get('[aria-current = true]').should('have.css', 'color', 'rgb(64, 76, 237)');
-
-//lougout
-cy.get('.user-info__title').click({force:true});
-cy.get('[id="logout-button"]').click({force:true});
-
-
-//next user admin
-cy.get('[id="loginForm.userId"]').click({force:true});
-cy.get('[aria-label="Dovilė Martinkutė"]').click();
-cy.get('[id="loginForm.role"]').click({force:true});
-cy.get('[aria-label="Admin"]').click();                          //change roles
-cy.get('[type="submit"]').click();
-
-cy.url().should('include', '/time-logging');         
-
-cy.get('.calendar').should('be.visible')
-cy.get('.tile.form').should('be.visible')
-cy.get('.user-info__title').contains('Dovile Martinkute');
-cy.get('.main-nav').find('li').should('have.length', 6);                //changes according to role
-
-
-    })
-})
+    }
+}) 
