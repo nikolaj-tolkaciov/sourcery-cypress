cc0b127fdd6dc6d51a3650dd3044ea6ce381393d
AvocadoGag <45439740+AvocadoGag@users.noreply.github.com>
2019-07-28 14:28:39 +0300

    Adding full feature functionality and ClientPage object update

--- cypress/integration/newClient.js
+++ cypress/integration/newClient.js
@@ -1,11 +1,9 @@
 import LoginPage from '../objects/loginPage';
 import TimeLoggingPage from '../objects/timeLoggingPage';
-import TasksPage from '../objects/tasksPage';
 import ClientPage from '../objects/ClientPage';
 
 const loginPage = new LoginPage();
 const timeLoggingPage = new TimeLoggingPage();
-const tasksPage = new TasksPage();
 const clientPage = new ClientPage();
 
 describe('Sourcebooks login', function() {
@@ -15,12 +13,36 @@ describe('Sourcebooks login', function() {
         loginPage.visit();
     })
 
+    let OrganisationName = Math.random().toString(36).substring(2, 15);
+    let ClientEmail = Math.random().toString(36).substring(2, 15) + '@Dungeon.yolo';
+
     it('Admin should create new client', function() {
 
+        //Client Page pasirinkimas
         timeLoggingPage.getNavigationPanelClients().should('be.visible');
         timeLoggingPage.getNavigationPanelClients().click({force:true});
+
+        // visu veiksmus Kad sukurti clienta atlikimas
         clientPage.getCreateClientsButton().should('be.visible');
         clientPage.getCreateClientsButton().click({force:true});
-       // clientPage.getCreateClientNameInputField().type(`${taskName}`); //parašyti
+        clientPage.getCreateOrganisationNameInputField().should('be.visible');
+        clientPage.getCreateOrganisationNameInputField().type(`${OrganisationName}`);
+        clientPage.getCreateClientFirstNameInputField().should('be.visible');
+        clientPage.getCreateClientFirstNameInputField().type('Welf');
+        clientPage.getCreateClientLastNameInputField().should('be.visible');
+        clientPage.getCreateClientLastNameInputField().type('Crozzo');
+        clientPage.getCreateClientEmailInputField().should('be.visible');
+        clientPage.getCreateClientEmailInputField().type(`${ClientEmail}`);
+        clientPage.getSaveButton().should('be.visible');
+        clientPage.getSaveButton().click({force:true});
+        
+        //Patikrinimas su filtravimu ar egzistuoja naujai sukurtas klientas
+        timeLoggingPage.getNavigationPanelClients().click({force:true});
+        clientPage.getOrganisationNameInputField().should('be.empty');
+        clientPage.getOrganisationNameInputField().type(`${OrganisationName}`);
+        clientPage.getClientEmailInputField().should('be.empty');
+        clientPage.getClientEmailInputField().type(`${ClientEmail}`);
+        clientPage.checkIfFilteredClientIsVisible(OrganisationName, ClientEmail);
+        
     })
 })
--- cypress/objects/ClientPage.js
+++ cypress/objects/ClientPage.js
@@ -1,13 +1,49 @@
 class ClientsPage {
 
-    //Clients
+    //Clients Page
 
     getCreateClientsButton() {
         return cy.get('[type="button"]').contains('Create Client');
     }
+
+    getOrganisationNameInputField() {
+        return cy.get('[type="text"]').eq(0);
+    }
+
+    getClientNameInputField() {
+        return cy.get('[type="text"]').eq(1);
+    }
+
+    getClientEmailInputField() {
+        return cy.get('[type="text"]').eq(2);
+    }
+
+    checkIfFilteredClientIsVisible(OrganisationName, ClientName) {
+        cy.get('.ellipsis').contains(OrganisationName).should('be.visible');
+        cy.get('.ellipsis').contains(ClientName).should('be.visible');
+    }
+
     //Clients Page => Create clients page
 
+    getCreateOrganisationNameInputField() {
+        return cy.get('[id="clientDetailsForm.organization"]')
+    }
 
+    getCreateClientFirstNameInputField() {
+        return cy.get('[id="clientDetailsForm.contacts_firstName_0"]')
+    }
+
+    getCreateClientLastNameInputField() {
+        return cy.get('[id="clientDetailsForm.contacts_lastName_0"]')
+    }
+
+    getCreateClientEmailInputField() {
+        return cy.get('[id="clientDetailsForm.contacts_email_0"]')
+    }
+
+    getSaveButton() {
+       return cy.get('[type=submit]').contains('Save');
+    }
 }
 
 export default ClientsPage
