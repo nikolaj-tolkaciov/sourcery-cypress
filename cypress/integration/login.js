b96474c54b78f66d62de5cfff3693be79ddd729e
Algirdas Viltrakis <viltrakis.a@gmail.com>
2019-07-31 16:34:49 +0300

    HW3

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -1,12 +1,14 @@
 import LoginPage from '../Objects/loginPage'
 import TimeLoginPage from '../Objects/ObjectTimeLogin/timeLoginPage'
+import TaskPageObj from '../Objects/ObTaskPage/TaskPageObj'
 
 const loginPage = new LoginPage()
 const timeLoginPage = new TimeLoginPage()
+const taskPageObj = new TaskPageObj()
 const testUser = 'Algirdas Viltrakis'
-
+const taskName = 'Kaip iškepti blynus 2'
 const rolesArray = [
-    {'role' : 'User', 'tabCount' : 1}, 
+    {'role' : 'User', 'tabCount' : 1},
     {'role' : 'Team Lead', 'tabCount' : 2},
     {'role' : 'Manager', 'tabCount' : 5}, 
     {'role' : 'Accountant', 'tabCount' : 5}, 
@@ -42,4 +44,27 @@ for (let role of rolesArray) {
         timeLoginPage.titleColour().should('have.css', 'color', 'rgb(64, 76, 237)')
     })
 }
+// Homework task 3.
+it('User login with role Admin, goes to Tasks and create New Task', function () {
+
+    loginPage.visit()
+    loginPage.getUser().click({force:true})
+    loginPage.getUserName(testUser).click()
+    loginPage.getRole().click({force:true})
+    loginPage.loginAdmin().click()
+    loginPage.submitLogin().click()
+// Creating task
+    taskPageObj.clickTask().click()
+    taskPageObj.createTask().click()
+    taskPageObj.taskName().type(taskName)
+    taskPageObj.typeDetails().type('Jami blynus ir kepi')
+    taskPageObj.billToClients().click()
+    taskPageObj.billYes().click()
+    taskPageObj.hourlyRate().clear().type('66.88')
+    taskPageObj.save().click()
+//Going to Tasks page to check if task is saved
+    taskPageObj.clickTask().click() 
+    taskPageObj.typeTaskName().type(taskName)
+    taskPageObj.checkTask().contains(taskName)
+})
 })
