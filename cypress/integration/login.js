26274b1bfcea6bc87ad17dafff20a6e77fd955fa
rasakap <rasa@ito.lt>
2019-08-01 14:49:43 +0300

    Fixed conflicts

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -29,7 +29,7 @@ describe('Sourcebooks login', function() {
         cy.get('[type="submit"]').click();
         cy.get('.Select.not-valid').should('be.visible')
     })
-    it('Should be able to login with role User', function () {
+    it('Should be able to login with role Tem Lead', function () {
         cy.visit('/');
         cy.get('[id="loginForm.userId"]').click({force:true});
         cy.get('[aria-label="Rasa Kapačinskaitė"]').click();
@@ -53,7 +53,7 @@ describe('Sourcebooks login', function() {
         cy.get('[type="submit"]').click();
         cy.get('.Select.not-valid').should('be.visible')
     })
-    it('Should be able to login with role User', function () {
+    it('Should be able to login with role Manager', function () {
         cy.visit('/');
         cy.get('[id="loginForm.userId"]').click({force:true});
         cy.get('[aria-label="Rasa Kapačinskaitė"]').click();
@@ -77,7 +77,7 @@ describe('Sourcebooks login', function() {
         cy.get('[type="submit"]').click();
         cy.get('.Select.not-valid').should('be.visible')
     })
-    it('Should be able to login with role User', function () {
+    it('Should be able to login with role Accountant', function () {
         cy.visit('/');
         cy.get('[id="loginForm.userId"]').click({force:true});
         cy.get('[aria-label="Rasa Kapačinskaitė"]').click();
@@ -101,7 +101,7 @@ describe('Sourcebooks login', function() {
         cy.get('[type="submit"]').click();
         cy.get('.Select.not-valid').should('be.visible')
     })
-    it('Should be able to login with role User', function () {
+    it('Should be able to login with role Admin', function () {
         cy.visit('/');
         cy.get('[id="loginForm.userId"]').click({force:true});
         cy.get('[aria-label="Rasa Kapačinskaitė"]').click();
@@ -113,7 +113,9 @@ describe('Sourcebooks login', function() {
         cy.get('.calendar').should('be.visible')
         cy.get('.tile.form').should('be.visible')
         cy.get('.user-info__title').contains('Rasa Kapacinskaite');
-        cy.get('.main-nav').find('li').should('have.length', 6);
-        cy.get('.main-nav__link--active').should('have.css','color', 'rgb(64, 76, 237)');       
+        cy.get('.main-nav').find('li').should('have.length', 6);        
+        cy.get('.main-nav__link--active').should('have.css','color', 'rgb(64, 76, 237)');   
+        let today = new Date();
+        cy.get('.calendar--today').find('.calendar__date').contains(today.getDate());    
     })
  })
