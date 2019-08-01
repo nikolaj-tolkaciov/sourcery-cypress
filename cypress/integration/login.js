b6558fdc72340230e3e87690f0c66176206f2aa1
EdvinasKveselys <edvinas.kveselys@gmail.com>
2019-08-01 13:50:03 +0300

    Removed duplicate test case; changed user to Edvinas Kveselys; changed role to Admin

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -8,36 +8,21 @@ describe('Sourcebooks login', function() {
         cy.get('.Select.not-valid').should('be.visible')
     })
 
-    it('Should be able to login with role User', function () {
+    it('Check login as Admin and if the day is today', function () {
 
         cy.visit('/');
         cy.get('[id="loginForm.userId"]').click({force:true});
-        cy.get('[aria-label="Demo User"]').click();
+        cy.get('[aria-label="Edvinas Kveselys"]').click();
         cy.get('[id="loginForm.role"]').click({force:true});
-        cy.get('[aria-label="Team Lead"]').click();
+        cy.get('[aria-label="Admin"]').click();
         cy.get('[type="submit"]').click();
 
         cy.url().should('include', '/time-logging');
         cy.get('.page__title').contains('Timesheets')
         cy.get('.calendar').should('be.visible')
         cy.get('.tile.form').should('be.visible')
-        cy.get('.user-info__title').contains('Demo User');
-        cy.get('.main-nav').find('li').should('have.length', 2);
-    })
-
-    it('Check if the day is today', function () {
-
-        cy.visit('/');
-        cy.get('[id="loginForm.userId"]').click({force:true});
-        cy.get('[aria-label="Demo User"]').click();
-        cy.get('[id="loginForm.role"]').click({force:true});
-        cy.get('[aria-label="Team Lead"]').click();
-        cy.get('[type="submit"]').click();
-
-        cy.url().should('include', '/time-logging');
-        cy.get('.page__title').contains('Timesheets')
-        cy.get('.calendar').should('be.visible')
-        cy.get('.tile.form').should('be.visible')
+        cy.get('.user-info__title').contains('Edvinas Kveselys');
+        cy.get('.main-nav').find('li').should('have.length', 6);
 
         const today = new Date();
         const day = today.getDate();
