b6ece3ff95bd2672486cce9b7a99b7f6b3b9c62a
Liepapa <53540818+Liepapa@users.noreply.github.com>
2019-08-01 12:55:41 +0300

    finish task1

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -23,5 +23,6 @@ describe('Sourcebooks login', function() {
         cy.get('.tile.form').should('be.visible')
         cy.get('.user-info__title').contains('Demo User');
         cy.get('.main-nav').find('li').should('have.length', 1);
+        cy.get('.calendar--today').find('.calendar__date').contains('1');
+
     })
-})
