7e53217f20a1617ef1f3beac4273418b91969902
ModestasGujis <gujismodestas@gmail.com>
2019-08-01 12:15:53 +0300

    Added T-3 task creations

---
+++ cypress/helpers/TaskPage.js
@@ -0,0 +1,21 @@
+export default {
+    visit: () => cy.visit('/tasks'),
+
+    getCreateButton: () => cy.contains('Create Task'),
+
+    getTaskNameInput: () => cy.get('[id="taskDetailsForm.name"]'),
+    
+    getDescriptionInput: () => cy.get('[id="taskDetailsForm.description"]'),
+
+    getBillToClient: () => cy.get('.Select-value'),
+
+    getBillToClientOption: (option) => cy.get('[aria-label="' + option + '"]'), // 'Yes' or 'No'
+
+    getHourlyRate: () => cy.get('[id="taskDetailsForm.rate"]'),
+
+    getSubmitButton: () => cy.get('[type="submit"]'),
+
+    getNameFilter: () => cy.get('.field__text.field__text--small').first(),
+
+    getTaskList: () => cy.get('.ag-body-container > div'),
+}
---
+++ cypress/integration/adminTasks.js
@@ -0,0 +1,36 @@
+import TaskPage from '../helpers/TaskPage'
+
+// T-3
+describe('Task creation', () => {
+
+    it('admin should be able to add new tasks', () => {
+        cy.loginAs('Admin')
+        TaskPage.visit()
+        TaskPage.getCreateButton().click()
+
+        cy.url().should('include', '/tasks/create');
+
+        
+        const randomName = Math.random().toString(36).substring(9);
+        TaskPage.getTaskNameInput().type(randomName)
+        TaskPage.getDescriptionInput().type(randomName)
+
+        const billToClient = TaskPage.getBillToClient()
+        billToClient.should('have.length', 1)
+        billToClient.click()
+        TaskPage.getBillToClientOption('Yes').click()
+
+        TaskPage.getHourlyRate().type(Math.random()*15)
+
+        TaskPage.getSubmitButton().click()
+        
+        cy.url().should('include', '/tasks')
+        cy.url().should('not.include', '/create')
+
+        TaskPage.visit()
+		TaskPage.getNameFilter().type(randomName)
+		TaskPage.getTaskList().should('have.length', 1)
+    })
+
+
+})
