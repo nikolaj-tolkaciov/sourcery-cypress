a719c08e3a4ad515445519ef9462ce69c9ed2df8
dominykasposkus <dominykasposkus@outlook.com>
2019-08-01 16:04:37 +0300

    Added some logic to ensure that "Time logging" tab is selected and is marked in blue color

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -29,46 +29,42 @@ describe('Sourcebooks login', function() {
         cy.get('.calendar--today').find('.calendar__date').contains((new Date()).getDate());
     })
 
-    let Roles = [
-        {
-            "name": "User",
-            "tabs": 1
-        },
-        {
-            "name": "Team Lead",
-            "tabs": 2
-        },
-        {
-            "name": "Manager",
-            "tabs": 5
-        },
-        {
-            "name": "Accountant",
-            "tabs": 5
-        }, 
-        {
-            "name": "Admin",
-            "tabs": 6
-        }];
+    it('Verify all user roles can log in and should see appropriate tabs', function(){
+        let Roles = [
+            {
+                "name":"User",
+                "tabs": 1
+            },
+            {
+                "name": "Team Lead",
+                "tabs":2
+            },
+            {
+                "name":"Manager",
+                "tabs":5
+            },
+            {
+                "name":"Accountant",
+                "tabs":5
+            }, 
+            {
+                "name":"Admin",
+                "tabs":6
+            }];
 
-    for(let i = 0; i < Roles.length; i++){
-        it('Verify that ' + Roles[i].name + ' can log in and should see ' + Roles[i].tabs + ' tabs', function(){
+        for(let i = 0; i < Roles.length; i++){
+            cy.visit('/');
+            cy.get('[id="loginForm.userId"]').click({force:true});
+            cy.get('[aria-label="Dominykas Poškus"]').click();
+            cy.get('[id="loginForm.role"]').click({force:true});
+            cy.get('[aria-label="'+ Roles[i].name +'"]').click();
+            cy.get('[type="submit"]').click();
+
+            cy.get('.user-info__title').contains('Dominykas Poškus');
+            cy.get('.main-nav').find('li').should('have.length', Roles[i].tabs);
             
-        cy.visit('/');
-        cy.get('[id="loginForm.userId"]').click({force:true});
-        cy.get('[aria-label="Dominykas Poškus"]').click();
-        cy.get('[id="loginForm.role"]').click({force:true});
-        cy.get('[aria-label="'+ Roles[i].name +'"]').click();
-        cy.get('[type="submit"]').click();
-
-        cy.get('.user-info__title').contains('Dominykas Poškus');
-        cy.get('.main-nav').find('li').should('have.length', Roles[i].tabs);
-        cy.get('.main-nav__link--active').contains("Time Logging");
-        cy.get('.main-nav__link--active').should('have.css', 'color').and('eq', 'rgb(64, 76, 237)');
-
-        cy.get('.user-info__title').click();
-        cy.get('[id="logout-button"]').click();
+            cy.get('.user-info__title').click();
+            cy.get('[id="logout-button"]').click();
         }
-    )
-}
+    })
 })
