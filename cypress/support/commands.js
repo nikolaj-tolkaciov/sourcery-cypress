a7f4148090cb85fecfcd4112632116ffa490234a
ntolkaciov <Nikolaj.Tolkaciov@devbridge.com>
2019-08-01 11:42:36 +0300

    Deleted unnecessary new line

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -10,4 +10,3 @@ Cypress.Commands.add('loginAs', (role) => {
     window.localStorage.setItem('USER_INFO', '{"id":1,"firstName":"Demo","lastName":"User"}')
     window.localStorage.setItem('ACCESS_TOKEN', `userId=1&${USER_ROLES_MAP[role]}`)
 })
-
