44a055a187511275803d67a591d016d33ca6ef5c
kendzy <modestaskmieliauskas@gmail.com>
2019-08-01 14:11:02 +0300

    Added color check

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -1,20 +1,15 @@
+//import './support/commands.js';
 describe('Sourcebooks login', function() {
-
-    it('Should display validation for empty user after attempted loggin', function () {
-        
-        cy.visit('/');
-        cy.get('.Select.not-valid').should('not.visible')
-        cy.get('[type="submit"]').click();
-        cy.get('.Select.not-valid').should('be.visible')
-    })
 
     let role = 'Team Lead';
     var today = new Date();
+    //let Roles = ['User','Team Lead','Manager','Accountant','Admin'];
+
     it('Should be able to login with role ' + role, function () {
         
         cy.visit('/');
         cy.get('[id="loginForm.userId"]').click({force:true});
-        cy.get('[aria-label="Demo User"]').click();
+        cy.get('[aria-label="Modestas Kmieliauskas"]').click();
         cy.get('[id="loginForm.role"]').click({force:true});
         cy.get(`[aria-label="${role}"]`).click();
         cy.get('[type="submit"]').click();
@@ -23,8 +18,9 @@ describe('Sourcebooks login', function() {
         cy.get('.page__title').contains('Timesheets')
         cy.get('.calendar').should('be.visible')
         cy.get('.tile.form').should('be.visible')
-        cy.get('.user-info__title').contains('Demo User');
+        cy.get('.user-info__title').contains('Modestas Kmieliauskas');
         cy.get('.main-nav').find('li').should('have.length', 2);
         cy.get('.calendar--today').contains(today.getDate());
+       cy.get('.main-nav__link--active').should('have.css','color','rgb(64, 76, 237)');
     })
 })
