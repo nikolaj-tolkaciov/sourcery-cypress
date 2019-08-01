c464d2627be7e622ce7bdcf01dd2f7464d93dd94
AzureAD\LauraJankauske <laura.jankauske@devbridge.com>
2019-08-01 15:56:38 +0300

    T2 verify roles

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -1,4 +1,8 @@
+import chaiColors from 'chai-colors';
+chai.use(chaiColors)
+
 describe('Sourcebooks login', function() {
+    const user='Demo User'
 
     it('Should display validation for empty user after attempted loggin', function () {
         
@@ -12,7 +16,7 @@ describe('Sourcebooks login', function() {
 
         cy.visit('/');
         cy.get('[id="loginForm.userId"]').click({force:true});
-        cy.get('[aria-label="Demo User"]').click();
+        cy.get(`[aria-label="${user}"]`).click();
         cy.get('[id="loginForm.role"]').click({force:true});
         cy.get('[aria-label="Team Lead"]').click();
         cy.get('[type="submit"]').click();
@@ -38,4 +42,28 @@ describe('Sourcebooks login', function() {
         const today = Cypress.moment().format('D')
         cy.get('.calendar--today').contains(today);
     })
+
+        let roles = ['User', 'Team Lead', 'Manager', 'Accountant', 'Admin']
+        let tabsAmount = [1, 2, 5, 5, 6];
+
+        for(let i=0; i< roles.length; i++ ){
+            it('User:' + roles[i] + ' logs in and can see ' + tabsAmount[i] + ' tab(s)', function () {
+                cy.visit('/');
+                cy.get('[id="loginForm.userId"]').click({force:true});
+                cy.get(`[aria-label="${user}"]`).click();
+                cy.get('[id="loginForm.role"]').click({force:true});
+                cy.get(`[aria-label="${roles[i]}"]`).click();
+                cy.get('[type="submit"]').click();
+
+                cy.url().should('include', '/time-logging');
+                cy.get('.page__title').contains('Timesheets');
+                cy.get('.user-info__title').contains(user);
+                cy.get('.main-nav').find('li').should('have.length', tabsAmount[i] );
+                cy.get('.main-nav__link--active').contains('Time Logging')
+                .should('have.css', 'color')
+                .and('be.colored', '#404ced')
+                cy.contains(user).click();
+                cy.contains('Log Out').click();
+            })
+         }
 })
