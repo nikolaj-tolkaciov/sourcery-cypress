cef1fe95881a9ec077a1eef54ab7f8c7f2d2fdcc
dominykasposkus <dominykasposkus@outlook.com>
2019-08-01 14:38:15 +0300

    Verify all user roles can log in and should see appropriate tabs

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -21,11 +21,50 @@ describe('Sourcebooks login', function() {
         cy.get('.page__title').contains('Timesheets');
         cy.get('.calendar').should('be.visible');
         cy.get('.tile.form').should('be.visible');
-        cy.get('.user-info__title').contains('Demo User');
+        cy.get('.user-info__title').contains('Dominykas Poškus');
         cy.get('.main-nav').find('li').should('have.length', 2);
     })
-    
+
     it('Calendar should show todays date', function(){
         cy.get('.calendar--today').find('.calendar__date').contains((new Date()).getDate());
+    })
+
+    it('Verify all user roles can log in and should see appropriate tabs', function(){
+        let Roles = [
+            {
+                "name":"User",
+                "tabs": 1
+            },
+            {
+                "name": "Team Lead",
+                "tabs":2
+            },
+            {
+                "name":"Manager",
+                "tabs":5
+            },
+            {
+                "name":"Accountant",
+                "tabs":5
+            }, 
+            {
+                "name":"Admin",
+                "tabs":6
+            }];
+
+        for(let i = 0; i < Roles.length; i++){
+            cy.visit('/');
+            cy.get('[id="loginForm.userId"]').click({force:true});
+            cy.get('[aria-label="Dominykas Poškus"]').click();
+            cy.get('[id="loginForm.role"]').click({force:true});
+            cy.get('[aria-label="'+ Roles[i].name +'"]').click();
+            cy.get('[type="submit"]').click();
+
+            cy.get('.user-info__title').contains('Dominykas Poškus');
+            cy.get('.main-nav').find('li').should('have.length', Roles[i].tabs);
+            
+            cy.get('.user-info__title').click();
+            cy.get('[id="logout-button"]').click();
+        }
     })
 })
