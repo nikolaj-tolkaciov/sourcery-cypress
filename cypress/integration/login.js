0641a56b0c42383ed0af6901de62c6df09e1a6b3
kendzy <modestaskmieliauskas@gmail.com>
2019-08-01 14:57:33 +0300

    Renamed variables

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -1,17 +1,17 @@
-//import './support/commands.js';
 describe('Sourcebooks login', function() {
-
-    let role = 'Team Lead';
+    
     var today = new Date();
-    //let Roles = ['User','Team Lead','Manager','Accountant','Admin'];
-
-    it('Should be able to login with role ' + role, function () {
+    const roles = ['User','Team Lead','Manager','Accountant','Admin'];
+    const Tabs = ['1','2','5','5','6'];
+    
+    for(let i = 0; i < roles.length;i++){
+    it('Should be able to login with role ' + roles[i] , function () {
         
         cy.visit('/');
         cy.get('[id="loginForm.userId"]').click({force:true});
         cy.get('[aria-label="Modestas Kmieliauskas"]').click();
         cy.get('[id="loginForm.role"]').click({force:true});
-        cy.get(`[aria-label="${role}"]`).click();
+        cy.get(`[aria-label="${roles[i]}"]`).click();
         cy.get('[type="submit"]').click();
 
         cy.url().should('include', '/time-logging');
@@ -19,8 +19,9 @@ describe('Sourcebooks login', function() {
         cy.get('.calendar').should('be.visible')
         cy.get('.tile.form').should('be.visible')
         cy.get('.user-info__title').contains('Modestas Kmieliauskas');
-        cy.get('.main-nav').find('li').should('have.length', 2);
+        cy.get('.main-nav').find('li').should('have.length', Tabs[i]);
         cy.get('.calendar--today').contains(today.getDate());
        cy.get('.main-nav__link--active').should('have.css','color','rgb(64, 76, 237)');
     })
+}
 })
