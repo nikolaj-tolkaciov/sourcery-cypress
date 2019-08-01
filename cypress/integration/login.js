5dfc72432694a1aa57a6b69b893733d78dc2cd23
Liepapa <53540818+Liepapa@users.noreply.github.com>
2019-08-01 14:41:49 +0300

    t2 roles + colour check

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -1,32 +1,134 @@
 describe('Sourcebooks login', function() {
 
     it('Should display validation for empty user after attempted loggin', function () {
-        
+ 
         cy.visit('/');
         cy.get('.Select.not-valid').should('not.visible')
         cy.get('[type="submit"]').click();
         cy.get('.Select.not-valid').should('be.visible')
     })
-
+ 
     it('Should be able to login with role User', function () {
-
+ 
         cy.visit('/');
         cy.get('[id="loginForm.userId"]').click({force:true});
-        cy.get('[aria-label="Demo User"]').click();
+        cy.get('[aria-label="Liepa Popescu"]').click();
         cy.get('[id="loginForm.role"]').click({force:true});
         cy.get('[aria-label="User"]').click();
         cy.get('[type="submit"]').click();
-
+ 
         cy.url().should('include', '/time-logging');
         cy.get('.page__title').contains('Timesheets')
         cy.get('.calendar').should('be.visible')
         cy.get('.tile.form').should('be.visible')
-        cy.get('.user-info__title').contains('Demo User');
+        cy.get('.user-info__title').contains('Liepa Popescu');
         cy.get('.main-nav').find('li').should('have.length', 1);
-
-        let today = new Date();
-
-        cy.get('.calendar--today').find('.calendar__date').contains(today.getDate());
+        cy.get('.main-nav__link--active').should('have.css','color', 'rgb(64, 76, 237)');
+ 
     })
-}) 
+    it('Should display validation for empty user after attempted loggin', function () {
+ 
+        cy.visit('/');
+        cy.get('.Select.not-valid').should('not.visible')
+        cy.get('[type="submit"]').click();
+        cy.get('.Select.not-valid').should('be.visible')
+    })
+ 
+    it('Should be able to login with role Team Lead', function () {
+ 
+        cy.visit('/');
+        cy.get('[id="loginForm.userId"]').click({force:true});
+        cy.get('[aria-label="Liepa Popescu"]').click();
+        cy.get('[id="loginForm.role"]').click({force:true});
+        cy.get('[aria-label="Team Lead"]').click();
+        cy.get('[type="submit"]').click();
+ 
+        cy.url().should('include', '/time-logging');
+        cy.get('.page__title').contains('Timesheets')
+        cy.get('.calendar').should('be.visible')
+        cy.get('.tile.form').should('be.visible')
+        cy.get('.user-info__title').contains('Liepa Popescu');
+        cy.get('.main-nav').find('li').should('have.length', 2);
+        cy.get('.main-nav__link--active').should('have.css','color', 'rgb(64, 76, 237)');
+ 
+    })
+    it('Should display validation for empty user after attempted loggin', function () {
+ 
+        cy.visit('/');
+        cy.get('.Select.not-valid').should('not.visible')
+        cy.get('[type="submit"]').click();
+        cy.get('.Select.not-valid').should('be.visible')
+    })
+ 
+    it('Should be able to login with role Manager', function () {
+ 
+        cy.visit('/');
+        cy.get('[id="loginForm.userId"]').click({force:true});
+        cy.get('[aria-label="Liepa Popescu"]').click();
+        cy.get('[id="loginForm.role"]').click({force:true});
+        cy.get('[aria-label="Manager"]').click();
+        cy.get('[type="submit"]').click();
+ 
+        cy.url().should('include', '/time-logging');
+        cy.get('.page__title').contains('Timesheets')
+        cy.get('.calendar').should('be.visible')
+        cy.get('.tile.form').should('be.visible')
+        cy.get('.user-info__title').contains('Liepa Popescu');
+        cy.get('.main-nav').find('li').should('have.length', 5);
+        cy.get('.main-nav__link--active').should('have.css','color', 'rgb(64, 76, 237)');
+ 
+    })
+    it('Should display validation for empty user after attempted loggin', function () {
+ 
+        cy.visit('/');
+        cy.get('.Select.not-valid').should('not.visible')
+        cy.get('[type="submit"]').click();
+        cy.get('.Select.not-valid').should('be.visible')
+    })
+ 
+    it('Should be able to login with role Accountant', function () {
+ 
+        cy.visit('/');
+        cy.get('[id="loginForm.userId"]').click({force:true});
+        cy.get('[aria-label="Liepa Popescu"]').click();
+        cy.get('[id="loginForm.role"]').click({force:true});
+        cy.get('[aria-label="Accountant"]').click();
+        cy.get('[type="submit"]').click();
+ 
+        cy.url().should('include', '/time-logging');
+        cy.get('.page__title').contains('Timesheets')
+        cy.get('.calendar').should('be.visible')
+        cy.get('.tile.form').should('be.visible')
+        cy.get('.user-info__title').contains('Liepa Popescu');
+        cy.get('.main-nav').find('li').should('have.length', 5);
+        cy.get('.main-nav__link--active').should('have.css','color', 'rgb(64, 76, 237)');
+ 
+    })
+    it('Should display validation for empty user after attempted loggin', function () {
+ 
+        cy.visit('/');
+        cy.get('.Select.not-valid').should('not.visible')
+        cy.get('[type="submit"]').click();
+        cy.get('.Select.not-valid').should('be.visible')
+    })
+ 
+    it('Should be able to login with role Admin', function () {
+ 
+        cy.visit('/');
+        cy.get('[id="loginForm.userId"]').click({force:true});
+        cy.get('[aria-label="Liepa Popescu"]').click();
+        cy.get('[id="loginForm.role"]').click({force:true});
+        cy.get('[aria-label="Admin"]').click();
+        cy.get('[type="submit"]').click();
+ 
+        cy.url().should('include', '/time-logging');
+        cy.get('.page__title').contains('Timesheets')
+        cy.get('.calendar').should('be.visible')
+        cy.get('.tile.form').should('be.visible')
+        cy.get('.user-info__title').contains('Liepa Popescu');
+        cy.get('.main-nav').find('li').should('have.length', 6);cy.get('.main-nav__link--active').should('have.css','color', 'rgb(64, 76, 237)');
+ 
+    })
+ })
+ 
 
