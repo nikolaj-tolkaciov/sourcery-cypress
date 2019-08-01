ebc5cdff6386a83271d6ff91da7672eb94692790
AzureAD\LauraJankauske <laura.jankauske@devbridge.com>
2019-08-01 16:21:57 +0300

    Formatting

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -43,27 +43,27 @@ describe('Sourcebooks login', function() {
         cy.get('.calendar--today').contains(today);
     })
 
-        let roles = ['User', 'Team Lead', 'Manager', 'Accountant', 'Admin']
-        let tabsAmount = [1, 2, 5, 5, 6];
+    let roles = ['User', 'Team Lead', 'Manager', 'Accountant', 'Admin']
+    let tabsAmount = [1, 2, 5, 5, 6];
 
-        for(let i=0; i< roles.length; i++ ){
-            it('User:' + roles[i] + ' logs in and can see ' + tabsAmount[i] + ' tab(s)', function () {
-                cy.visit('/');
-                cy.get('[id="loginForm.userId"]').click({force:true});
-                cy.get(`[aria-label="${user}"]`).click();
-                cy.get('[id="loginForm.role"]').click({force:true});
-                cy.get(`[aria-label="${roles[i]}"]`).click();
-                cy.get('[type="submit"]').click();
+    for(let i=0; i< roles.length; i++ ){
+        it('User:' + roles[i] + ' logs in and can see ' + tabsAmount[i] + ' tab(s)', function () {
+            cy.visit('/');
+            cy.get('[id="loginForm.userId"]').click({force:true});
+            cy.get(`[aria-label="${user}"]`).click();
+            cy.get('[id="loginForm.role"]').click({force:true});
+            cy.get(`[aria-label="${roles[i]}"]`).click();
+            cy.get('[type="submit"]').click();
 
-                cy.url().should('include', '/time-logging');
-                cy.get('.page__title').contains('Timesheets');
-                cy.get('.user-info__title').contains(user);
-                cy.get('.main-nav').find('li').should('have.length', tabsAmount[i] );
-                cy.get('.main-nav__link--active').contains('Time Logging')
-                .should('have.css', 'color')
-                .and('be.colored', '#404ced')
-                cy.contains(user).click();
-                cy.contains('Log Out').click();
-            })
-         }
+            cy.url().should('include', '/time-logging');
+            cy.get('.page__title').contains('Timesheets');
+            cy.get('.user-info__title').contains(user);
+            cy.get('.main-nav').find('li').should('have.length', tabsAmount[i] );
+            cy.get('.main-nav__link--active').contains('Time Logging')
+            .should('have.css', 'color')
+            .and('be.colored', '#404ced')
+            cy.contains(user).click();
+            cy.contains('Log Out').click();
+        })
+    }
 })
