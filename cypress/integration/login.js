3ad8a5e18970a7671c62ff14374547d39e9b58de
Ieva Stonkaitė <i.stonkaite@gmail.com>
2019-08-01 14:53:46 +0300

    T2 finished task

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -26,7 +26,77 @@ describe('Sourcebooks login', function() {
     })
 
     it('Should show calendar date', function () {
-        cy.get('.calendar--selected').find('span').contains(1);
+        cy.get('.calendar--selected').find('span').contains(new Date().getDate());
+    })
+
+    it('Should verify all user roles can log in and should see appropriate tabs - USER', function () {
+    
+        cy.visit('/');
+        cy.get('[id="loginForm.userId"]').click({force:true});
+        cy.get('[aria-label="Ieva Stonkaitė"]').click();
+        cy.get('[id="loginForm.role"]').click({force:true});
+        cy.get('[aria-label="User"]').click();
+        cy.get('[type="submit"]').click();
+        cy.get('.user-info__title').contains('Ieva Stonkaite');
+        cy.get('.main-nav').find('li').should('have.length', 1); //tabu skaicius
+        cy.url().should('include', '/time-logging');
+        cy.get('.main-nav__link--active').should('have.css', 'color', 'rgb(64, 76, 237)');        
+    })
+
+    it('Should verify all user roles can log in and should see appropriate tabs - TEAM LEAD', function () {
+    
+        cy.visit('/');
+        cy.get('[id="loginForm.userId"]').click({force:true});
+        cy.get('[aria-label="Ieva Stonkaitė"]').click();
+        cy.get('[id="loginForm.role"]').click({force:true});
+        cy.get('[aria-label="Team Lead"]').click();
+        cy.get('[type="submit"]').click();
+        cy.get('.user-info__title').contains('Ieva Stonkaite');
+        cy.get('.main-nav').find('li').should('have.length', 2); //tabu skaicius
+        cy.url().should('include', '/time-logging');
+        cy.get('.main-nav__link--active').should('have.css', 'color', 'rgb(64, 76, 237)');        
+    })
+
+    it('Should verify all user roles can log in and should see appropriate tabs - MANAGER', function () {
+    
+        cy.visit('/');
+        cy.get('[id="loginForm.userId"]').click({force:true});
+        cy.get('[aria-label="Ieva Stonkaitė"]').click();
+        cy.get('[id="loginForm.role"]').click({force:true});
+        cy.get('[aria-label="Manager"]').click();
+        cy.get('[type="submit"]').click();
+        cy.get('.user-info__title').contains('Ieva Stonkaite');
+        cy.get('.main-nav').find('li').should('have.length', 5); //tabu skaicius
+        cy.url().should('include', '/time-logging');
+        cy.get('.main-nav__link--active').should('have.css', 'color', 'rgb(64, 76, 237)');        
+    })
+
+    it('Should verify all user roles can log in and should see appropriate tabs - ACCOUNTANT', function () {
+    
+        cy.visit('/');
+        cy.get('[id="loginForm.userId"]').click({force:true});
+        cy.get('[aria-label="Ieva Stonkaitė"]').click();
+        cy.get('[id="loginForm.role"]').click({force:true});
+        cy.get('[aria-label="Accountant"]').click();
+        cy.get('[type="submit"]').click();
+        cy.get('.user-info__title').contains('Ieva Stonkaite');
+        cy.get('.main-nav').find('li').should('have.length', 5); //tabu skaicius
+        cy.url().should('include', '/time-logging');
+        cy.get('.main-nav__link--active').should('have.css', 'color', 'rgb(64, 76, 237)');        
+    })
+
+    it('Should verify all user roles can log in and should see appropriate tabs - ADMIN', function () {
+    
+        cy.visit('/');
+        cy.get('[id="loginForm.userId"]').click({force:true});
+        cy.get('[aria-label="Ieva Stonkaitė"]').click();
+        cy.get('[id="loginForm.role"]').click({force:true});
+        cy.get('[aria-label="Admin"]').click();
+        cy.get('[type="submit"]').click();
+        cy.get('.user-info__title').contains('Ieva Stonkaite');
+        cy.get('.main-nav').find('li').should('have.length', 6); //tabu skaicius
+        cy.url().should('include', '/time-logging');
+        cy.get('.main-nav__link--active').should('have.css', 'color', 'rgb(64, 76, 237)');        
     })
 
 })
