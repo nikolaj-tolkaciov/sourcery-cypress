bd4902129a5fd817bdaf28ba3f820778942100e7
tortonas <valentinas.kasteckis@gmail.com>
2019-08-01 12:29:44 +0100

    added uuser and manager

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -30,7 +30,40 @@ describe('Sourcebooks login', function() {
         cy.get('.calendar--selected').find('span').get(".calendar__date").contains(d.getDate());
     })
 
-  
+    it('I can login as a User', function(){
+        cy.visit('/');
+        cy.get('[id="loginForm.userId"]').click({force:true});
+        cy.get('[aria-label="Valentinas Kasteckis"]').click();
+        cy.get('[id="loginForm.role"]').click({force:true});
+        cy.get('[aria-label="User"]').click();
+        cy.get('[type="submit"]').click();
+
+        cy.url().should('include', '/time-logging');
+        cy.get('.page__title').contains('Timesheets')
+        cy.get('.calendar').should('be.visible')
+        cy.get('.tile.form').should('be.visible')
+        cy.get('.user-info__title').contains('Valentinas Kasteckis');
+        cy.get('.main-nav').find('li').should('have.length', 1);
+        cy.get('main-nav__link--active').should("color", "#404ced");
+
+    })   
+
+    it('I can login as a Manager', function(){
+        
+        cy.visit('/');
+        cy.get('[id="loginForm.userId"]').click({force:true});
+        cy.get('[aria-label="Valentinas Kasteckis"]').click();
+        cy.get('[id="loginForm.role"]').click({force:true});
+        cy.get('[aria-label="Manager"]').click();
+        cy.get('[type="submit"]').click();
+
+        cy.url().should('include', '/time-logging');
+        cy.get('.page__title').contains('Timesheets')
+        cy.get('.calendar').should('be.visible')
+        cy.get('.tile.form').should('be.visible')
+        cy.get('.user-info__title').contains('Valentinas Kasteckis');
+        cy.get('.main-nav').find('li').should('have.length', 5);
+    })
 
 
 })
