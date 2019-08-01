78b8ab5b4eebab8601ffddf4ecf463c9be995aff
mindaugas_mc <mindaugas_mc@yahoo.com>
2019-08-01 14:46:46 +0300

    T-2: Verify if all user roles can log in and see appropriate tabs

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -11,8 +11,7 @@ describe('Sourcebooks login', function() {
     cy.get('[id="loginForm.userId"]').click({ force: true });
     cy.get('[aria-label="Demo User"]').click();
     cy.get('[id="loginForm.role"]').click({ force: true });
-    // cy.get('[aria-label="User"]').click();
-    cy.get('[aria-label="Team Lead"]').click();
+    cy.get('[aria-label="User"]').click();
     cy.get('[type="submit"]').click();
 
     cy.url().should('include', '/time-logging');
@@ -34,7 +33,108 @@ describe('Sourcebooks login', function() {
     cy.get('[type="submit"]').click();
 
     cy.visit('/time-logging');
-    let date = new Date();
-    cy.get('.calendar--today.calendar--selected').contains(date.getDate());
+    cy.get('.calendar--today.calendar--selected').contains(
+      new Date().getDate()
+    );
+  });
+
+  it('Verify role User, if it can log in and see appropriate tabs', function() {
+    cy.visit('/');
+    cy.get('[id="loginForm.userId"]').click({ force: true });
+    cy.get('[aria-label="Mindaugas Maceika"]').click();
+    cy.get('[id="loginForm.role"]').click({ force: true });
+    cy.get('[aria-label="User"]').click();
+    cy.get('[type="submit"]').click();
+
+    cy.url().should('include', '/time-logging');
+    cy.get('.page__title').contains('Timesheets');
+    cy.get('.calendar').should('be.visible');
+    cy.get('.user-info__title').contains('Mindaugas Maceika');
+    cy.get('.main-nav')
+      .find('li')
+      .should('have.length', 1);
+    cy.get('.main-nav__link--active')
+      .should('have.css', 'color')
+      .and('eq', 'rgb(64, 76, 237)');
+  });
+
+  it('Verify role Team Lead, if it can log in and see appropriate tabs', function() {
+    cy.visit('/');
+    cy.get('[id="loginForm.userId"]').click({ force: true });
+    cy.get('[aria-label="Mindaugas Maceika"]').click();
+    cy.get('[id="loginForm.role"]').click({ force: true });
+    cy.get('[aria-label="Team Lead"]').click();
+    cy.get('[type="submit"]').click();
+
+    cy.url().should('include', '/time-logging');
+    cy.get('.page__title').contains('Timesheets');
+    cy.get('.calendar').should('be.visible');
+    cy.get('.user-info__title').contains('Mindaugas Maceika');
+    cy.get('.main-nav')
+      .find('li')
+      .should('have.length', 2);
+    cy.get('.main-nav__link--active')
+      .should('have.css', 'color')
+      .and('eq', 'rgb(64, 76, 237)');
+  });
+
+  it('Verify role Manager, if it can log in and see appropriate tabs', function() {
+    cy.visit('/');
+    cy.get('[id="loginForm.userId"]').click({ force: true });
+    cy.get('[aria-label="Mindaugas Maceika"]').click();
+    cy.get('[id="loginForm.role"]').click({ force: true });
+    cy.get('[aria-label="Manager"]').click();
+    cy.get('[type="submit"]').click();
+
+    cy.url().should('include', '/time-logging');
+    cy.get('.page__title').contains('Timesheets');
+    cy.get('.calendar').should('be.visible');
+    cy.get('.user-info__title').contains('Mindaugas Maceika');
+    cy.get('.main-nav')
+      .find('li')
+      .should('have.length', 5);
+    cy.get('.main-nav__link--active')
+      .should('have.css', 'color')
+      .and('eq', 'rgb(64, 76, 237)');
+  });
+
+  it('Verify role Accountant, if it can log in and see appropriate tabs', function() {
+    cy.visit('/');
+    cy.get('[id="loginForm.userId"]').click({ force: true });
+    cy.get('[aria-label="Mindaugas Maceika"]').click();
+    cy.get('[id="loginForm.role"]').click({ force: true });
+    cy.get('[aria-label="Accountant"]').click();
+    cy.get('[type="submit"]').click();
+
+    cy.url().should('include', '/time-logging');
+    cy.get('.page__title').contains('Timesheets');
+    cy.get('.calendar').should('be.visible');
+    cy.get('.user-info__title').contains('Mindaugas Maceika');
+    cy.get('.main-nav')
+      .find('li')
+      .should('have.length', 5);
+    cy.get('.main-nav__link--active')
+      .should('have.css', 'color')
+      .and('eq', 'rgb(64, 76, 237)');
+  });
+
+  it('Verify role Admin, if it can log in and see appropriate tabs', function() {
+    cy.visit('/');
+    cy.get('[id="loginForm.userId"]').click({ force: true });
+    cy.get('[aria-label="Mindaugas Maceika"]').click();
+    cy.get('[id="loginForm.role"]').click({ force: true });
+    cy.get('[aria-label="Admin"]').click();
+    cy.get('[type="submit"]').click();
+
+    cy.url().should('include', '/time-logging');
+    cy.get('.page__title').contains('Timesheets');
+    cy.get('.calendar').should('be.visible');
+    cy.get('.user-info__title').contains('Mindaugas Maceika');
+    cy.get('.main-nav')
+      .find('li')
+      .should('have.length', 6);
+    cy.get('.main-nav__link--active')
+      .should('have.css', 'color')
+      .and('eq', 'rgb(64, 76, 237)');
   });
 });
