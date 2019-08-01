95901a6ce70318dad5f015142f5fa38ee9159083
tortonas <valentinas.kasteckis@gmail.com>
2019-08-01 12:08:35 +0100

    fixed, added get

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -27,9 +27,10 @@ describe('Sourcebooks login', function() {
 
     it('Date should match todays date', function() {
         var d = new Date();
-        cy.get('.calendar--selected').find('span').contains(d.getDate());
-       // cy.get('.calendar--selected').find('span').contains(3.00);
-
-
+        cy.get('.calendar--selected').find('span').get(".calendar__date").contains(d.getDate());
     })
+
+  
+
+
 })
