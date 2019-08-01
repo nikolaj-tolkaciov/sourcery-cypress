c619989a454a05ed80bdab2316a4e2dd4e97bf8a
saulerau <sraudyte@gmail.com>
2019-08-01 22:11:34 +0300

    unstable code fixed

--- cypress/integration/create.js
+++ cypress/integration/create.js
@@ -2,6 +2,8 @@ describe('Sourcebooks login', function() {
 
     it('Admin should create a new task', function () {
 
+        const taskName = Math.ceil((Math.random())*10000);
+
         cy.visit('/');
         cy.get('[id="loginForm.userId"]').click({force:true});
         cy.get('[aria-label="Saulė Raudytė"]').click();
@@ -11,7 +13,7 @@ describe('Sourcebooks login', function() {
 
         cy.get('[href="/tasks"]').click();
         cy.get('.btn').click();
-        cy.get('[id="taskDetailsForm.name"]').type("Simple Sample7");
+        cy.get('[id="taskDetailsForm.name"]').type(taskName);
         cy.get('[id="taskDetailsForm.description"]').type("Writing first automated test on thursday.");
         cy.get('[aria-selected="true"]').click({ force: true }).get('[aria-label="Yes"]').click({ force: true });
         cy.get('[id="taskDetailsForm.rate"]').clear().type("5.000");
@@ -21,8 +23,7 @@ describe('Sourcebooks login', function() {
         cy.url().should('to.not.equal',('/tasks/create'));
         
         cy.visit('/tasks');
-        cy.get('.field__text').first().click().type("Simple Sample7");
-        cy.get('[title="Simple Sample7"]').should('exist');
-        cy.get('[row-index="1"]').should('not.exist');
+        cy.get('.field__text').first().click().type(taskName);
+        cy.get('[title=' + taskName + ']').should('exist');
     })
 })
