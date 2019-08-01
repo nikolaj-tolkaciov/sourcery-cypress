fade8ff9a3d74e2d6bf041592968b5c16c19e785
DainiusGaidam <dainius.gaidamavicius@gmail.com>
2019-08-01 16:21:21 +0300

    deleted t3 from login

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -46,22 +46,5 @@ describe('Sourcebooks login', function() {
             cy.get('.main-nav__link--active').should('have.css','color', 'rgb(64, 76, 237)');
         })
     })  
-
-   /* it('T-3: Admin creates new task', function () {
-        cy.visit('/tasks');
-        cy.get('[id="loginForm.userId"]').click({force:true});
-        cy.get('[aria-label="Dainius Gaidamavičius"]').click();
-        cy.get('[id="loginForm.role"]').click({force:true});
-        cy.get('[aria-label="Admin"]').click()
-        cy.get('[type="submit"]').click();
-        cy.get('[type="button"]').click();
-        cy.get('[id = "taskDetailsForm.name"]').type('TASK1');
-        cy.get('[id = "taskDetailsForm.description"]').type('TASK11');
-        cy.get('[id = "react-select-8--value-item]').select('Yes')
-    }) */
-
-
-
-
-
+ 
 })
