42d55788f164736059253de0124ebe875b68e5be
JauniusM <jauniusmykolaitis@gmail.com>
2019-08-01 15:02:26 +0300

    T-2 verify all user roles with 'Your user'

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -8,11 +8,11 @@ describe('Sourcebooks login', function() {
         cy.get('.Select.not-valid').should('be.visible')
     })
 
-    it('Should be able to login with role User', function () {
+    it('Should be able to login with user Jaunius_Mykolaitis to role User', function () {
 
         cy.visit('/');
         cy.get('[id="loginForm.userId"]').click({force:true});
-        cy.get('[aria-label="Demo User"]').click();
+        cy.get('[aria-label="Jaunius Mykolaitis"]').click();
         cy.get('[id="loginForm.role"]').click({force:true});
         cy.get('[aria-label="User"]').click();
         cy.get('[type="submit"]').click();
@@ -21,9 +21,90 @@ describe('Sourcebooks login', function() {
         cy.get('.page__title').contains('Timesheets')
         cy.get('.calendar').should('be.visible')
         cy.get('.tile.form').should('be.visible')
-        cy.get('.user-info__title').contains('Demo User');
+        cy.get('.user-info__title').contains('Jaunius Mykolaitis');
         cy.get('.main-nav').find('li').should('have.length', 1);
 
-        cy.get('.calendar--today').find('span').contains('1');
+        cy.get('.main-nav__link--active').should('have.css', 'color').and('equal', 'rgb(64, 76, 237)');
+        cy.get('.calendar--today').find('span').contains(new Date().getDate());
+    })
+
+    it('Should be able to login with user Jaunius_Mykolaitis to role Admin', function () {
+
+        cy.visit('/');
+        cy.get('[id="loginForm.userId"]').click({force:true});
+        cy.get('[aria-label="Jaunius Mykolaitis"]').click();
+        cy.get('[id="loginForm.role"]').click({force:true});
+        cy.get('[aria-label="Admin"]').click();
+        cy.get('[type="submit"]').click();
+
+        cy.url().should('include', '/time-logging');
+        cy.get('.page__title').contains('Timesheets')
+        cy.get('.calendar').should('be.visible')
+        cy.get('.tile.form').should('be.visible')
+        cy.get('.user-info__title').contains('Jaunius Mykolaitis');
+        cy.get('.main-nav').find('li').should('have.length', 6);
+
+        cy.get('.main-nav__link--active').should('have.css', 'color').and('equal', 'rgb(64, 76, 237)');
+        cy.get('.calendar--today').find('span').contains(new Date().getDate());
+    })
+
+    it('Should be able to login with user Jaunius_Mykolaitis to role Team Lead', function () {
+
+        cy.visit('/');
+        cy.get('[id="loginForm.userId"]').click({force:true});
+        cy.get('[aria-label="Jaunius Mykolaitis"]').click();
+        cy.get('[id="loginForm.role"]').click({force:true});
+        cy.get('[aria-label="Team Lead"]').click();
+        cy.get('[type="submit"]').click();
+
+        cy.url().should('include', '/time-logging');
+        cy.get('.page__title').contains('Timesheets')
+        cy.get('.calendar').should('be.visible')
+        cy.get('.tile.form').should('be.visible')
+        cy.get('.user-info__title').contains('Jaunius Mykolaitis');
+        cy.get('.main-nav').find('li').should('have.length', 2);
+
+        cy.get('.main-nav__link--active').should('have.css', 'color').and('equal', 'rgb(64, 76, 237)');
+        cy.get('.calendar--today').find('span').contains(new Date().getDate());
+    })
+
+    it('Should be able to login with user Jaunius_Mykolaitis to role Manager', function () {
+
+        cy.visit('/');
+        cy.get('[id="loginForm.userId"]').click({force:true});
+        cy.get('[aria-label="Jaunius Mykolaitis"]').click();
+        cy.get('[id="loginForm.role"]').click({force:true});
+        cy.get('[aria-label="Manager"]').click();
+        cy.get('[type="submit"]').click();
+
+        cy.url().should('include', '/time-logging');
+        cy.get('.page__title').contains('Timesheets')
+        cy.get('.calendar').should('be.visible')
+        cy.get('.tile.form').should('be.visible')
+        cy.get('.user-info__title').contains('Jaunius Mykolaitis');
+        cy.get('.main-nav').find('li').should('have.length', 5);
+
+        cy.get('.main-nav__link--active').should('have.css', 'color').and('equal', 'rgb(64, 76, 237)');
+        cy.get('.calendar--today').find('span').contains(new Date().getDate());
+    })
+    
+    it('Should be able to login with user Jaunius_Mykolaitis to role Accountant', function () {
+
+        cy.visit('/');
+        cy.get('[id="loginForm.userId"]').click({force:true});
+        cy.get('[aria-label="Jaunius Mykolaitis"]').click();
+        cy.get('[id="loginForm.role"]').click({force:true});
+        cy.get('[aria-label="Accountant"]').click();
+        cy.get('[type="submit"]').click();
+
+        cy.url().should('include', '/time-logging');
+        cy.get('.page__title').contains('Timesheets')
+        cy.get('.calendar').should('be.visible')
+        cy.get('.tile.form').should('be.visible')
+        cy.get('.user-info__title').contains('Jaunius Mykolaitis');
+        cy.get('.main-nav').find('li').should('have.length', 5);
+
+        cy.get('.main-nav__link--active').should('have.css', 'color').and('equal', 'rgb(64, 76, 237)');
+        cy.get('.calendar--today').find('span').contains(new Date().getDate());
     })
 })
