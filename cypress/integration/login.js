217f5c365d8ef4ad2a431dbc06b33328ad011c7e
kendzy <modestaskmieliauskas@gmail.com>
2019-08-02 11:28:00 +0300

    Refactored tests to use PageObjects

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -1,28 +1,47 @@
+import LoginPage from "../objects/loginPage";
+import TimeLoggingPage from "../objects/timeLoggingPage";
+
+const loginPage = new LoginPage();
+const timeLoggingPage = new TimeLoggingPage();
+
 describe('Sourcebooks login', function () {
 
     var today = new Date();
+    const userName = 'Modestas Kmieliauskas';
+    const urlTimeLogging = '/time-logging';
+    const pageTitle = 'Timesheets';
     const roles = ['User', 'Team Lead', 'Manager', 'Accountant', 'Admin'];
     const Tabs = ['1', '2', '5', '5', '6'];
+    const activeTabColor = 'rgb(64, 76, 237)';
+
+    it('Should display validation for empty user after attempted loggin', function () {
+
+        loginPage.visit();
+        loginPage.getUserValidationIndicator().should('not.visible')
+        loginPage.getSubmitButton().click();
+        loginPage.getUserValidationIndicator().should('be.visible')
+    })
 
     for (let i = 0; i < roles.length; i++) {
         it('Should be able to login with role ' + roles[i] +
-            ' , check current day and active tab color', function () {
+            ', check current day and active tab color', function () {
 
-                cy.visit('/');
-                cy.get('[id="loginForm.userId"]').click({ force: true });
-                cy.get('[aria-label="Modestas Kmieliauskas"]').click();
-                cy.get('[id="loginForm.role"]').click({ force: true });
-                cy.get(`[aria-label="${roles[i]}"]`).click();
-                cy.get('[type="submit"]').click();
+                loginPage.visit();
+                loginPage.getUserDropDown().click({ force: true });
+                loginPage.getUserOption(userName).click();
+                loginPage.getRoleDropDown().click({ force: true });
+                loginPage.getRoleOption(roles[i]).click();
+                loginPage.getSubmitButton().click();
 
-                cy.url().should('include', '/time-logging');
-                cy.get('.page__title').contains('Timesheets');
-                cy.get('.calendar').should('be.visible');
-                cy.get('.tile.form').should('be.visible');
-                cy.get('.user-info__title').contains('Modestas Kmieliauskas');
-                cy.get('.main-nav').find('li').should('have.length', Tabs[i]);
-                cy.get('.calendar--today').contains(today.getDate());
-                cy.get('.main-nav__link--active').should('have.css', 'color', 'rgb(64, 76, 237)');
+
+                timeLoggingPage.assertUrl(urlTimeLogging);
+                timeLoggingPage.getPageTitle().contains('Timesheets');
+                timeLoggingPage.getCalendarSelector().should('be.visible');
+                timeLoggingPage.getTileForm().should('be.visible');
+                timeLoggingPage.getUserInfoTitle().contains(userName);
+                timeLoggingPage.getMainNav().find('li').should('have.length', Tabs[i]);
+                timeLoggingPage.getCalendarToday().contains(today.getDate());
+                timeLoggingPage.getActiveMainNav().should('have.css', 'color', activeTabColor);
             })
     }
 })
