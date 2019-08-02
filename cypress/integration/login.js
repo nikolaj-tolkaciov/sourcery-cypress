c97349861d2b710475fbcc5b542e4e79b2cd0129
tortonas <valentinas.kasteckis@gmail.com>
2019-08-02 11:02:28 +0300

    refactored code

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -1,122 +1,95 @@
+import LoginPage from '../obj/loginPage';
+
+const loginPage = new LoginPage();
+
 describe('Sourcebooks login', function() {
 
     it('Should display validation for empty user after attempted loggin', function () {
         
-        cy.visit('/');
-        cy.get('.Select.not-valid').should('not.visible')
-        cy.get('[type="submit"]').click();
-        cy.get('.Select.not-valid').should('be.visible')
+        loginPage.visit();
+        loginPage.getUserValidationIndicator().should('not.visible')
+        loginPage.getSubmitButton().click();
+        loginPage.getUserValidationIndicator().should('be.visible')
     })
 
     it('Should be able to login with role Team Lead', function () {
+        loginPage.visit();
+        loginPage.openUpUserDropDown().click({force:true});
+        loginPage.getSpecificUserFromDropDown("Valentinas Kasteckis").click();
+        loginPage.openUpRoleDropDown().click({force:true});
+        loginPage.getSpecificRoleFromDropDown("Team Lead").click();
+        loginPage.getSubmitButton().click();
 
-        cy.visit('/');
-        cy.get('[id="loginForm.userId"]').click({force:true});
-        cy.get('[aria-label="Valentinas Kasteckis"]').click();
-        cy.get('[id="loginForm.role"]').click({force:true});
-        cy.get('[aria-label="Team Lead"]').click();
-        cy.get('[type="submit"]').click();
-
-        cy.url().should('include', '/time-logging');
-        cy.get('.page__title').contains('Timesheets')
-        cy.get('.calendar').should('be.visible')
-        cy.get('.tile.form').should('be.visible')
-        cy.get('.user-info__title').contains('Valentinas Kasteckis');
-        cy.get('.main-nav').find('li').should('have.length', 2);
+        loginPage.checkIfWeAreInSpecificURL('/time-logging');
+        loginPage.getPageTitle().contains('Timesheets');
+        loginPage.getCalendar().should('be.visible')
+        loginPage.getTitleForm().should('be.visible')
+        loginPage.checkIfInfoTitleIsSpecific("Valentinas Kasteckis");
+        loginPage.checkNavigationMenuLength(2);
     })
 
     it('Date should match todays date', function() {
-        var d = new Date();
-        cy.get('.calendar--today').find('span').get(".calendar__date").contains(d.getDate());    
+        
+        loginPage.checkIfTodaysDateIsCorrect();
     })
 
     it('I can login as a User', function(){
-        cy.visit('/');
-        cy.get('[id="loginForm.userId"]').click({force:true});
-        cy.get('[aria-label="Valentinas Kasteckis"]').click();
-        cy.get('[id="loginForm.role"]').click({force:true});
-        cy.get('[aria-label="User"]').click();
-        cy.get('[type="submit"]').click();
-
-        cy.url().should('include', '/time-logging');
-        cy.get('.page__title').contains('Timesheets')
-        cy.get('.calendar').should('be.visible')
-        cy.get('.tile.form').should('be.visible')
-        cy.get('.user-info__title').contains('Valentinas Kasteckis');
-        cy.get('.main-nav').find('li').should('have.length', 1);
-        cy.get('.main-nav__link--active').should('have.css', 'color', 'rgb(64, 76, 237)');
-
+        loginPage.visit();
+        loginPage.loginAsSpecificUserAndRole("Valentinas Kasteckis", "User");
+        loginPage.checkIfWeAreInSpecificURL('/time-logging');
+        loginPage.getPageTitle().contains('Timesheets')
+        loginPage.getCalendar().should('be.visible')
+        loginPage.getTitleForm().should('be.visible')
+        loginPage.checkIfInfoTitleIsSpecific("Valentinas Kasteckis");
+        loginPage.checkNavigationMenuLength(1);
+        loginPage.checkSelectedButtonColor();
     })   
 
     it('I can login as a Team Lead', function(){
-        
-        cy.visit('/');
-        cy.get('[id="loginForm.userId"]').click({force:true});
-        cy.get('[aria-label="Valentinas Kasteckis"]').click();
-        cy.get('[id="loginForm.role"]').click({force:true});
-        cy.get('[aria-label="Team Lead"]').click();
-        cy.get('[type="submit"]').click();
-
-        cy.url().should('include', '/time-logging');
-        cy.get('.page__title').contains('Timesheets')
-        cy.get('.calendar').should('be.visible')
-        cy.get('.tile.form').should('be.visible')
-        cy.get('.user-info__title').contains('Valentinas Kasteckis');
-        cy.get('.main-nav').find('li').should('have.length', 2);
-        cy.get('.main-nav__link--active').should('have.css', 'color', 'rgb(64, 76, 237)');
+        loginPage.visit();
+        loginPage.loginAsSpecificUserAndRole("Valentinas Kasteckis", "Team Lead");
+        loginPage.checkIfWeAreInSpecificURL('/time-logging');
+        loginPage.getPageTitle().contains('Timesheets')
+        loginPage.getCalendar().should('be.visible')
+        loginPage.getTitleForm().should('be.visible')
+        loginPage.checkIfInfoTitleIsSpecific("Valentinas Kasteckis");
+        loginPage.checkNavigationMenuLength(2);
+        loginPage.checkSelectedButtonColor();
     })
 
     it('I can login as a Manager', function(){
-        
-        cy.visit('/');
-        cy.get('[id="loginForm.userId"]').click({force:true});
-        cy.get('[aria-label="Valentinas Kasteckis"]').click();
-        cy.get('[id="loginForm.role"]').click({force:true});
-        cy.get('[aria-label="Manager"]').click();
-        cy.get('[type="submit"]').click();
-
-        cy.url().should('include', '/time-logging');
-        cy.get('.page__title').contains('Timesheets')
-        cy.get('.calendar').should('be.visible')
-        cy.get('.tile.form').should('be.visible')
-        cy.get('.user-info__title').contains('Valentinas Kasteckis');
-        cy.get('.main-nav').find('li').should('have.length', 5);
-        cy.get('.main-nav__link--active').should('have.css', 'color', 'rgb(64, 76, 237)');
+        loginPage.visit();
+        loginPage.loginAsSpecificUserAndRole("Valentinas Kasteckis", "Manager");
+        loginPage.checkIfWeAreInSpecificURL('/time-logging');
+        loginPage.getPageTitle().contains('Timesheets')
+        loginPage.getCalendar().should('be.visible')
+        loginPage.getTitleForm().should('be.visible')
+        loginPage.checkIfInfoTitleIsSpecific("Valentinas Kasteckis");
+        loginPage.checkNavigationMenuLength(5);
+        loginPage.checkSelectedButtonColor();
     })
 
     it('I can login as a Accountant', function(){
-        
-        cy.visit('/');
-        cy.get('[id="loginForm.userId"]').click({force:true});
-        cy.get('[aria-label="Valentinas Kasteckis"]').click();
-        cy.get('[id="loginForm.role"]').click({force:true});
-        cy.get('[aria-label="Accountant"]').click();
-        cy.get('[type="submit"]').click();
-
-        cy.url().should('include', '/time-logging');
-        cy.get('.page__title').contains('Timesheets')
-        cy.get('.calendar').should('be.visible')
-        cy.get('.tile.form').should('be.visible')
-        cy.get('.user-info__title').contains('Valentinas Kasteckis');
-        cy.get('.main-nav').find('li').should('have.length', 5);
-        cy.get('.main-nav__link--active').should('have.css', 'color', 'rgb(64, 76, 237)');
+        loginPage.visit();
+        loginPage.loginAsSpecificUserAndRole("Valentinas Kasteckis", "Accountant");
+        loginPage.checkIfWeAreInSpecificURL('/time-logging');
+        loginPage.getPageTitle().contains('Timesheets')
+        loginPage.getCalendar().should('be.visible')
+        loginPage.getTitleForm().should('be.visible')
+        loginPage.checkIfInfoTitleIsSpecific("Valentinas Kasteckis");
+        loginPage.checkNavigationMenuLength(5);
+        loginPage.checkSelectedButtonColor();
     })
 
     it('I can login as a Admin', function(){
-        
-        cy.visit('/');
-        cy.get('[id="loginForm.userId"]').click({force:true});
-        cy.get('[aria-label="Valentinas Kasteckis"]').click();
-        cy.get('[id="loginForm.role"]').click({force:true});
-        cy.get('[aria-label="Admin"]').click();
-        cy.get('[type="submit"]').click();
-
-        cy.url().should('include', '/time-logging');
-        cy.get('.page__title').contains('Timesheets')
-        cy.get('.calendar').should('be.visible')
-        cy.get('.tile.form').should('be.visible')
-        cy.get('.user-info__title').contains('Valentinas Kasteckis');
-        cy.get('.main-nav').find('li').should('have.length', 6);
-        cy.get('.main-nav__link--active').should('have.css', 'color', 'rgb(64, 76, 237)');
+        loginPage.visit();
+        loginPage.loginAsSpecificUserAndRole("Valentinas Kasteckis", "Admin");
+        loginPage.checkIfWeAreInSpecificURL('/time-logging');
+        loginPage.getPageTitle().contains('Timesheets')
+        loginPage.getCalendar().should('be.visible')
+        loginPage.getTitleForm().should('be.visible')
+        loginPage.checkIfInfoTitleIsSpecific("Valentinas Kasteckis");
+        loginPage.checkNavigationMenuLength(6);
+        loginPage.checkSelectedButtonColor();
     })
 })
