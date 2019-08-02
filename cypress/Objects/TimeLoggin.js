c55fac660e7461ca02f901d461878e903060bcf4
JauniusM <jauniusmykolaitis@gmail.com>
2019-08-02 12:41:43 +0300

    Refactored code T2

--- cypress/Objects/TimeLoggin.js
+++ cypress/Objects/TimeLoggin.js
@@ -17,6 +17,12 @@ class TimeLoggin{
     getMainNavWithSpecificLength(){
         return cy.get('.main-nav').find('li')
     }
+    getMainNavActiveLinkColor(){
+        return cy.get('.main-nav__link--active').should('have.css', 'color').and('equal', 'rgb(64, 76, 237)')
+    }
+    getTodaysDay(){
+        return cy.get('.calendar--today').find('span').contains(new Date().getDate())
+    }
 }
 
 export default TimeLoggin
