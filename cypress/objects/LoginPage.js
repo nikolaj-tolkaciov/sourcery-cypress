27257b7945aaf5bdecd43cc0f68dec4769216f48
rasakap <rasa@ito.lt>
2019-08-02 13:12:23 +0300

    LoginPage.js added

---
+++ cypress/objects/LoginPage.js
@@ -0,0 +1,63 @@
+class LoginPage {
+    visit() {
+        cy.visit('/');
+    }
+
+    getUserValidationIndicator() {
+        return cy.get('.Select.not-valid');
+    }
+    getSubmitButton() {
+        return cy.get('[type="submit"]');
+    }
+
+    getSpecificUserFromDropDown(userName) {
+        return cy.get(`[aria-label="${userName}"]`)
+    }
+
+    getUserIdForm() {
+        return cy.get('[id="loginForm.userId"]')
+    }
+
+    getUserRoleForm() {
+        return cy.get('[id="loginForm.role"]')
+    }
+
+    getUserRoleFromDropDown(userRole) {
+        return cy.get(`[aria-label="${userRole}"]`)
+    }
+
+    url() {
+        return cy.url()
+    }
+
+    getPageTitle() {
+        return cy.get('.page__title')
+    }
+
+    getItemCalendar() {
+        return cy.get('.calendar')
+    }
+
+    getFormTitle() {
+        return cy.get('.tile.form')
+    }
+
+    getUserInfoTitle(userName) {
+        return cy.get('.user-info__title')
+    }
+
+    getMainNavigationBar() {
+        return cy.get('.main-nav')
+    }
+
+    getActiveTabLink() {
+        return cy.get('.main-nav__link--active')
+    }
+
+    getCalendarDayToday() {
+        return cy.get('.calendar--today')
+    }
+ }
+
+ export default LoginPage;
+
