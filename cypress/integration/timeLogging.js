28af12aeab7937765f51c2c7b463b664803a395d
MariusLastauskas <marius20122@gmail.com>
2019-08-05 14:17:38 +0300

    time logging testing added

--- cypress/integration/timeLogging.js
+++ cypress/integration/timeLogging.js
@@ -1,11 +1,55 @@
 import TimeLoggingPage from '../obj/timeLogging/timeLoggingPage';
 import LoginPage from '../obj/loginPage';
+import Common from '../obj/common';
 
 const timeLoggingPage = new TimeLoggingPage();
 const loginPage = new LoginPage();
+const common = new Common();
 
 describe('Sourcebooks testTimeLogging', function() {
-    it('Should be displaying todays date in Time Logging page', function() {
+    it('Should be available for Admin to log time entry', function () {
+        cy.loginAs("Admin");
+        loginPage.visit();
+        
+        let project = 'pro';
+        let task = '04';
+        let description1 = `description1_${common.makeId()}`;
+        let description2 = `description2_${common.makeId()}`;
+        let hoursLogged = 0.1;
+
+        timeLoggingPage.getCalendarToday().click();
+        timeLoggingPage.getProjectDropdown().click();
+        timeLoggingPage.getProjectFromDropdown(project).click();
+        timeLoggingPage.getTaskDropdown().click();
+        timeLoggingPage.getTaskFromDropdown(task).click();
+        timeLoggingPage.getFormDescription().type(description1);
+        timeLoggingPage.getFormHours().type(hoursLogged);
+        timeLoggingPage.getSubmitButton().click();
+
+        timeLoggingPage.getAllTimeLogsCheckbox().click({force: true});
+        timeLoggingPage.getDeleteButton().click({force: true});
+        timeLoggingPage.getDeleteConfirmButton().click();
+
+        timeLoggingPage.getFormDescription().type(description1);
+        timeLoggingPage.getFormHours().type(hoursLogged);
+        timeLoggingPage.getSubmitButton().click();
+        timeLoggingPage.getTodayHoursRecord().contains(hoursLogged.toFixed(2));
+        timeLoggingPage.getLoggedTimes().should('have.length', 1);
+        timeLoggingPage.getFirstLoggedTimeProject(project).contains(project);
+        timeLoggingPage.getFirstLoggedTimeTask().contains(task);
+        timeLoggingPage.getFirstLoggedTimeDescription(description1).contains(description1);
+        timeLoggingPage.getFirstLoggedTimeHours().contains(hoursLogged.toFixed(2));
+
+        timeLoggingPage.getFormDescription(description2).type(description2);
+        timeLoggingPage.getFormHours().type(hoursLogged);
+        timeLoggingPage.getSubmitButton().click();
+        timeLoggingPage.getTodayHoursRecord().contains((hoursLogged * 2).toFixed(2));
+        timeLoggingPage.getLoggedTimes().should('have.length', 2);
+
+
+    })
+
+    it('Should be displaying todays date in Time Logging page', function () {
         cy.loginAs("Admin");
         loginPage.visit();
         const today = new Date();
--- cypress/obj/timeLogging/timeLoggingPage.js
+++ cypress/obj/timeLogging/timeLoggingPage.js
@@ -14,6 +14,91 @@ class TimeLoggingPage {
     getCalendarToday() {
         return cy.get('.calendar--today').find('.calendar__date');
     }
+
+    getProjectDropdown() {
+        return cy.get('[class="Select is-searchable Select--single"]').first();
+    }
+
+    getProjectFromDropdown(projectName) {
+        return cy.get(`[aria-label="${projectName}"]`);
+    }
+
+    getTaskDropdown() {
+        return cy.get('[class="Select is-searchable Select--single"]').last();
+    }
+
+    getTaskFromDropdown(taskName) {
+        return cy.get(`[aria-label="${taskName}"]`);
+    }
+
+    getFormDescription() {
+        return cy.get('[id="timeLoggingForm.notes"]');
+    }
+
+    getFormHours() {
+        return cy.get('[id="timeLoggingForm.hours"]');
+    }
+
+    getSubmitButton() {
+        return cy.get('[type="submit"]');
+    }
+
+    getCalendarTile() {
+        return cy.get('.calendar--today');
+    }
+
+    getTodayHoursRecord() {
+        return this.getCalendarTile().find('.calendar__label');
+    }
+
+    getAllTimeLogsCheckbox() {
+        return cy.get('[class="ag-icon ag-icon-checkbox-unchecked"]').first();
+    }
+
+    getDeleteButton() {
+        return cy.get('[class="btn btn__secondary"]');
+    }
+
+    getDeleteConfirmButton() {
+        return cy.get('.btn__alert');
+    }
+
+    getLoggedTimes() {
+        return cy.get('[class="ag-body-viewport-wrapper"').find('.ag-body-viewport').find('.ag-body-container').children();
+    }
+
+    getFirstLoggedTimeProject(project) {
+        return cy.get(`[title="${project}"]`).first().parent().parent().should('have.attr', 'col-id', 'projectName');
+    }
+
+    getLastLoggedTimeProject(project) {
+        return cy.get(`[title="${project}"]`).last().parent().parent().should('have.attr', 'col-id', 'projectName');        
+    }
+
+    getFirstLoggedTimeTask() {
+        return cy.get('.ag-body-container').find('[col-id="taskName"]');        
+    }
+
+    getLastLoggedTimeTask(task) {
+        return cy.get('.ag-body-container').last().find('[col-id="taskName"]');   
+    }
+
+    getFirstLoggedTimeDescription(description) {
+        return cy.get(`[title="${description}"]`).first().parent().parent().should('have.attr', 'col-id', 'notes');        
+    }
+
+    getLastLoggedTimeDescription(description) {
+        return cy.get(`[title="${description}"]`).last().parent().parent().should('have.attr', 'col-id', 'notes');        
+    }
+
+    getFirstLoggedTimeHours() {
+        return cy.get('.time-list__hours').first();        
+    }
+
+    getLastLoggedTimeHours() {
+        return cy.get('.time-list__hours').last();        
+    }
+    
 }
 
 export default TimeLoggingPage;
