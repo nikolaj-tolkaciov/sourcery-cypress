a8e77c92352f984a600d3f89c7e3e4a5945562fb
Olga Pikul <olgapikul@gmail.com>
2019-08-05 15:46:17 +0300

    no message

--- cypress/objects/loginPage.js
+++ cypress/objects/loginPage.js
@@ -3,39 +3,51 @@ class LoginPage {
   visit() {
     cy.visit('/')
   }
+
   getValidationError() {
     return cy.get('Select.not-valid');
   }
+
   submitLoginButton() {
     return cy.get('[type="submit"]');
   }
+
   getValidation() {
     return cy.get('.Select.not-valid');
   }
+
   getUserOption(userName) {
     return cy.get(`[aria-label="${userName}"]`);
   }
+
   getUserId() {
     return cy.get('[id="loginForm.userId"]');
   }
+
   selectUserId() {
     return cy.get('[id="loginForm.userId"]');
   }
+
   getRole() {
     return cy.get('[id="loginForm.role"]');
   }
+
   roleLabelUser() {
     return cy.get('[aria-label="User"]');
   }
+
   roleLabelTeam() {
     return cy.get('[aria-label="Team Lead"]');
   }
+
   roleLabelManager() {
     return cy.get('[aria-label="Manager"]');
   }
+
   roleLabelAccountant() {
     return cy.get('[aria-label="Accountant"]');
   }
+  
   roleLabelAdmin() {
     return cy.get('[aria-label="Admin"]');
   }
