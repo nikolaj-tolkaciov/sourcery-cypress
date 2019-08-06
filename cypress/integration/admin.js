796cf6228c4cbee2e86c6b2e3d166c3d999f4455
mindaugas_mc <mindaugas_mc@yahoo.com>
2019-08-06 15:27:11 +0300

    Random task generation and random hourly rate implemented

--- cypress/integration/admin.js
+++ cypress/integration/admin.js
@@ -15,27 +15,36 @@ describe('Tests related to Admin role', function() {
     taskPage.getCreateTaskButton().click();
 
     cy.url().should('include', 'tasks/create');
+
+    let min = 0;
+    let max = 100;
+    let random = Math.random() * (+max - +min) + +min;
+    let randomTask = Math.floor(random);
+    let randomRate = random.toFixed(2);
+
     taskPage
       .getTaskNameInput()
-      .type('Admin task ')
-      .should('have.value', 'Admin task 3');
+      .type('Admin task ' + randomTask)
+      .should('have.value', 'Admin task ' + randomTask);
     taskPage
       .getTaskDescriptionInput()
-      .type('Description of admin task')
-      .should('have.value', 'Description of admin task');
+      .type('Description of admin task ' + randomTask)
+      .should('have.value', 'Description of admin task ' + randomTask);
     taskPage.getBillToClientDropDown().click();
     taskPage.getBillToClientDropDownOption('Yes').click();
     taskPage
       .getHourlyRate()
       .clear()
-      .type(Math.random() * 100);
+      .type(randomRate);
     taskPage.getSaveButton().click();
 
     taskPage.visit();
     cy.url().should('include', '/tasks');
     cy.url().should('not.include', '/create');
-    taskPage.getNameFilter().type('Admin task 2');
-    taskPage.getDescriptionFilter().type('Description of admin task');
+    taskPage.getNameFilter().type('Admin task ' + randomTask);
+    taskPage
+      .getDescriptionFilter()
+      .type('Description of admin task ' + randomTask);
     taskPage.getTaskList().should('have.length', 1);
   });
 });
