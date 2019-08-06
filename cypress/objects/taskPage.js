ddd1231a4e4f1702a69f2ea248ee5815fb20ddc4
Olga Pikul <olgapikul@gmail.com>
2019-08-06 11:40:03 +0300

    task page functionality

---
+++ cypress/objects/taskPage.js
@@ -0,0 +1,47 @@
+class TasksPage {
+
+ 
+    getTaskNameInputField() {
+        return cy.get('[type="text"]').eq(0);
+    }
+
+    getTaskDescriptionInputField() {
+        return cy.get('[type="text"]').eq(1);
+    }
+
+    checkIfFilteredTaskIsVisible(taskName) {
+        return cy.get('.ellipsis' + `[title=${taskName}]`);
+    }
+
+ 
+    getCreateTaskButton() {
+        return cy.get('[type="button"]').contains('Create Task');
+    }
+
+    getCreateTaskNameInputField() {
+        return cy.get('[id="taskDetailsForm.name"]');
+    }
+
+    getCreateTaskDescriptionInputField() {
+        return cy.get('[id="taskDetailsForm.description"]');
+    }
+
+    getBillableDropdown() {
+        return cy.get('.Select-value');
+    }
+
+    getYesFromBillableDropdown() {
+        return cy.get('[role="option"]').contains("Yes");
+    }
+
+    getBillableInputField() {
+        return cy.get('[id="taskDetailsForm.rate"]');
+    }
+
+    getSaveButton() {
+        return cy.get('[type=submit]').contains('Save');
+    }
+
+}
+
+export default TasksPage
