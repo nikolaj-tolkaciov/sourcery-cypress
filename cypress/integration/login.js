d9bd6c4e080438ea7d2c8e45266723a005d00aa2
Lansing12 <benediktas.gilis@gmail.com>
2019-08-14 10:05:13 +0300

    Some code lines names cahnged

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -1,27 +1,30 @@
 import LoginPage from '../objects/loginPage'
+import CreateTask from '../objects/createTask'
 const loginPage = new LoginPage()
+const createTask = new CreateTask()
 
 describe('Sourcebooks login', function() {
-
+    
     it('Admin creates a new task', function () {
+      
         loginPage.visit()
         loginPage.getLoginForm().click({force:true});
         loginPage.getUser().click();
         loginPage.getLoginRole().click({force:true});
         loginPage.getUserRole().click();
-        loginPage.getSubmit().click();
-        loginPage.getTasks().click();
-        loginPage.getCreateTasks().click();
-        loginPage.getDetailsForm().click({force:true});
-        loginPage.getTypeTaskName();
-        loginPage.getDescriptionForm().click({force:true});
-        loginPage.getTypeDescription();
-        loginPage.getBill().click({force:true});
-        loginPage.getBillable().click();
-        loginPage.getTypeRate();
-        loginPage.getSubmit().click();
-        loginPage.getTasks().click();
-        loginPage.getSearch();
-        loginPage.getValidate("Testavimas").contains('Tesatvimas');
+        loginPage.submit().click();
+        createTask.getTasks().click();
+        createTask.createTask().click();
+        createTask.getDetailsForm().click({force:true});
+        createTask.typeTaskName()
+        createTask.getDescriptionForm().click({force:true});
+        createTask.typeDescription()
+        createTask.setBill_to_Client().click({force:true});
+        createTask.selectBill_to_Client().click();
+        createTask.typeRate();
+        loginPage.submit().click();
+        createTask.getTasks().click();
+        createTask.search_for_Tasks();
+        createTask.check_TaskName_Exist();
     })
 })
