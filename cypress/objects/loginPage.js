e3db27719cee4ceacbb85026b94655abcd31c96b
Lansing12 <benediktas.gilis@gmail.com>
2019-08-14 10:05:23 +0300

    Some code lines transferred to createTask page

--- cypress/objects/loginPage.js
+++ cypress/objects/loginPage.js
@@ -2,26 +2,7 @@ class LoginPage {
     visit() {
         cy.visit('/')
     }
-
-/*function makeid(l)
-{
-var text = "";
-var char_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-for(var i=0; i < l; i++ )
-{  
-text += char_list.charAt(Math.floor(Math.random() * char_list.length));
-}
-return text;
-}
-    getValidationError () {
-        return cy.get('.Select.not-valid')
-    }
-    
-    getUserOption (userName) {
-        return cy.get(`[aria-label="${userName}"]`)
-    }
-    */
-   getSubmit () {
+    submit () {
     return cy.get('[type="submit"]')
     }
     getLoginForm () {
@@ -36,41 +17,6 @@ return text;
     getUserRole () {
         return cy.get(`[aria-label="Admin"]`)
     }
-    getTasks () {
-        return cy.contains('Tasks');
-    }
-    getCreateTasks () {
-        return cy.contains('Create Task');
-    }
-    getDetailsForm () {
-        return cy.get(`[id="taskDetailsForm.name"]`)
-    }
-    getTypeTaskName () {
-        return cy.get(`[id="taskDetailsForm.name"]`).type('Testavimas')
-    }
-    getTypeDescription () {
-        return cy.get(`[id="taskDetailsForm.description"]`).type('labas')
-    }
-    getDescriptionForm () {
-        return cy.get(`[id="taskDetailsForm.description"]`)
-    }
-    getBill () {
-        return cy.get(`[aria-selected="true"]`)
-    }
-    getBillable () {
-        return cy.get(`[aria-label="Yes"]`)
-    }
-    getTypeRate () {
-        return cy.get(`[id="taskDetailsForm.rate"]`).clear().type(Math.floor(Math.random()*10))
-    }
-    getSearch () {
-        return cy.get(`[class="field__text field__text--small"]`).first().click().type('Testavimas')
-    }
-    getValidate (Testavimas) {
-        return cy.get(`[title="${Testavimas}"]`)
-    }
-
-    
 } 
 
 export default LoginPage
