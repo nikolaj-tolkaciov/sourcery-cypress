c06a049724a75e77a32fb598f098a54e859ac4a3
kendzy <modestaskmieliauskas@gmail.com>
2019-08-27 14:55:35 +0300

    Configured for devbstaging lunch website. Added automated login, dish selection and validation.

---
+++ cypress/integration/automatedTests.js
@@ -0,0 +1,30 @@
+import LoginPage from "../objects/loginPage";
+import MainPage from "../objects/mainPage";
+
+const loginPage = new LoginPage();
+const mainPage = new MainPage();
+
+describe('devbstaging automated testing', function () {
+    const email = 'modestas@yellow.kns';
+    const passwd = 'Submarine124';
+    const mainDishName = 'Lazanija';
+    const soupName = 'Sriuba';
+
+    it('Should be able to login and select dish', function () {
+
+        // Login
+        cy.visit('/');
+        loginPage.getEmailField().click().type(email);
+        loginPage.getPasswordField().click().type(passwd);
+        loginPage.getLoginButton().click();
+
+        // Select weekday (1 equals Monday, 2 - Tuesday...)
+        mainPage.getWeekday(5).click();
+
+        // Select dish and verify selection
+        mainPage.getMainDish().contains(mainDishName).click();
+        mainPage.getSelectedItemsList().contains(mainDishName);
+        mainPage.getSoup().contains(soupName).click();
+        mainPage.getSelectedItemsList().contains(soupName);
+    })
+})
---
+++ cypress/objects/mainPage.js
@@ -0,0 +1,17 @@
+class LoginPage {
+    
+    getWeekday(weekday) {
+        return cy.get(`:nth-child(${weekday}) > .v-list__group__header > .v-list__group__header__append-icon > .v-icon`);
+    }
+    getMainDish() {
+        return cy.get(':nth-child(4) > .layout > .dish-card > .v-card__text');
+    }
+    getSoup() {
+        return cy.get(':nth-child(2) > .layout > .dish-card > .v-card__text');
+    }
+    getSelectedItemsList() {
+        return cy.get('.d-flex > .layout > .flex');
+    }
+}
+
+export default LoginPage;
